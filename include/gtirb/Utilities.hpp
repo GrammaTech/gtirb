4dddeb4e299985380567a4551a2c557a1a9c0962
jfarrier <jfarrier@grammatech.com>
2018-04-13 09:28:33 -0400

    Added missing DLL Exports.

--- include/gtirb/Utilities.hpp
+++ include/gtirb/Utilities.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <cstdint>
+#include <gtirb/Export.hpp>
 #include <vector>
 
 namespace gtirb
 {
     namespace util
     {
-        std::vector<uint16_t> ByteArray8To16(const std::vector<uint8_t>& x, bool swap = false);
+        GTIRB_GTIRB_EXPORT_API std::vector<uint16_t> ByteArray8To16(const std::vector<uint8_t>& x, bool swap = false);
     }
-}
+} // namespace gtirb
