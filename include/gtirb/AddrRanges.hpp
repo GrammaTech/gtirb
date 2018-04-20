572b18d68ceaf4b8178cd5b520eec6c9ff18d74a
John Farrier <jfarrier@grammatech.com>
2018-04-20 11:56:31 -0400

    Reimplementation of AddrRanges.

--- include/gtirb/AddrRanges.hpp
+++ include/gtirb/AddrRanges.hpp
@@ -3,7 +3,7 @@
 #include <cstdint>
 #include <gtirb/Node.hpp>
 #include <utility>
-#include <vector>
+#include <map>
 
 namespace gtirb
 {
@@ -12,6 +12,10 @@ namespace gtirb
     ///
     /// \class AddrRanges
     /// \author John E. Farrier
+    ///
+    /// Thin list of non-overlapping non-adjacent EA ranges.  Each range is [lb, ub), inclusive of
+    /// lb but exclusive of ub. This class guarantees a canonical representation: no redundancy or
+    /// overlaps, adjacent ranges are coalesced, empty ranges are excluded.
     ///
     class GTIRB_GTIRB_EXPORT_API AddrRanges final : public Node
     {
@@ -27,34 +31,93 @@ namespace gtirb
         virtual ~AddrRanges() = default;
 
         ///
-        /// Add a pair of EA's to the internal vector.
+        /// Add a pair of EA's to the ranges.
+        ///
         /// This will perform a check on the EA's to ensure that the first is less than or equal to
         /// the second.
         ///
         /// Throws a gtirb::RuntimeError if the EA's are not properly ordered.
         ///
-        /// \param  x   A pair of EA's to add to the vector.
+        /// \param  x   A pair of EA's to add to the container. (LB, UB)
         ///
-        void addRange(std::pair<gtirb::EA, gtirb::EA> x);
+        /// \return     True on success.
+        ///
+        bool addRange(std::pair<gtirb::EA, gtirb::EA> x);
 
         ///
-        /// Get all of the ranges that have been added.
+        /// Add (union) x into this range.
         ///
-        std::vector<std::pair<gtirb::EA, gtirb::EA>>& getRangeVector();
+        /// \return     True on success.
+        ///
+        bool addRange(const AddrRanges& x);
 
         ///
-        /// Get all of the ranges that have been added.
+        /// Subtract a pair of EA's from the ranges.
         ///
-        const std::vector<std::pair<gtirb::EA, gtirb::EA>>& getRangeVector() const;
+        /// This will perform a check on the EA's to ensure that the first is less than or equal to
+        /// the second.
+        ///
+        /// Throws a gtirb::RuntimeError if the EA's are not properly ordered.
+        ///
+        /// \param  x   A pair of EA's to subtract from the container. (LB, UB)
+        ///
+        bool subtractRange(std::pair<gtirb::EA, gtirb::EA> x);
+
+        ///
+        ///
+        ///
+        bool subtractRange(const AddrRanges& x);
 
         ///
         /// Given all of the ranges, how many bytes exist between all pairs?
         ///
-        /// \return     The number of bytes in all of the ranges in the vector.
+        /// \return     The number of bytes in all of the ranges in the container.
         ///
         size_t getBytesCoveredByRanges() const;
 
+        ///
+        ///
+        ///
+        bool getContains(gtirb::EA x) const;
+
+        ///
+        ///
+        ///
+        void clearRanges();
+
+        ///
+        ///
+        ///
+        void swap(gtirb::AddrRanges& x);
+
+        ///
+        /// Return an iterator pointing to the first region that intersects with the given [lb,ub).
+        ///
+        /// \return std::end(std::map<gtirb::EA, gtirb::EA>) if there is no intersection.
+        ///
+        std::map<gtirb::EA, gtirb::EA>::const_iterator getFirstIntersecting(gtirb::EA lb, gtirb::EA ub) const;
+
+        ///
+        ///
+        ///
+        std::map<gtirb::EA, gtirb::EA>::const_iterator getRangeContaining(gtirb::EA x) const;
+
+        ///
+        /// Use this to "destructively retrieve" this AddrRanges' map.
+        ///
+        void swapAndClear(std::map<gtirb::EA, gtirb::EA>& x);
+
+        ///
+        /// Use this to iterate through ranges
+        ///
+        std::map<gtirb::EA, gtirb::EA>& data();
+
+        ///
+        /// Use this to iterate through ranges
+        ///
+        const std::map<gtirb::EA, gtirb::EA>& data() const;
+
     private:
-        std::vector<std::pair<gtirb::EA, gtirb::EA>> rangeVector;
+        std::map<gtirb::EA, gtirb::EA> ranges;
     };
 }
