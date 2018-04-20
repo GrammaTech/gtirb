076f516ce85fa05edfec6983cac9a925ffd3c9ac
John Farrier <jfarrier@grammatech.com>
2018-04-20 08:20:58 -0400

    CFGNode Testing.

--- src/CFGNode.cpp
+++ src/CFGNode.cpp
@@ -40,44 +40,27 @@ CFGNode::Kind CFGNode::getKind() const
 
 void CFGNode::addSuccessor(CFGNode* x, bool isExecutable)
 {
-    auto sharedNode = std::dynamic_pointer_cast<CFGNode>(x->shared_from_this());
-    assert(sharedNode != nullptr);
-    this->successors.push_back({sharedNode, isExecutable});
+    this->add(this->successors, x, isExecutable);
 }
 
 void CFGNode::addSuccessor(std::unique_ptr<CFGNode>&& x, bool isExecutable)
 {
-    auto xPtr = x.get();
-    this->push_back(std::move(x));
-    this->addSuccessor(xPtr, isExecutable);
+    this->add(this->successors, std::move(x), isExecutable);
 }
 
 void CFGNode::setSuccessor(size_t index, CFGNode* x, bool isExecutable)
 {
-    auto sharedNode = std::dynamic_pointer_cast<CFGNode>(x->shared_from_this());
-
-    if(index < this->successors.size())
-    {
-        this->successors[index] = {sharedNode, isExecutable};
-    }
-    else
-    {
-        throw std::out_of_range(
-            "Attempt to set a successor CFGNode by an index outside the range of successors.");
-    }
+    this->set(this->successors, index, x, isExecutable);
 }
 
 void CFGNode::setSuccessor(size_t index, std::unique_ptr<CFGNode>&& x, bool isExecutable)
 {
-    auto xPtr = x.get();
-    this->push_back(std::move(x));
-    this->setSuccessor(index, xPtr, isExecutable);
+    this->set(this->successors, index, std::move(x), isExecutable);
 }
 
 std::pair<CFGNode*, bool> CFGNode::getSuccessor(size_t x) const
 {
-    auto s = this->successors[x];
-    return {s.first.lock().get(), s.second};
+    return this->get(this->successors, x);
 }
 
 bool CFGNode::getSuccessorsEmpty() const
@@ -92,60 +75,42 @@ size_t CFGNode::getSuccessorSize() const
 
 void CFGNode::removeSuccessor(size_t x)
 {
-    auto idx = std::begin(this->successors);
-    std::advance(idx, x);
-    this->successors.erase(idx);
+    this->remove(this->successors, x);
 }
 
 void CFGNode::removeSuccessor(const CFGNode* const x)
 {
-    this->successors.erase(
-        std::remove_if(std::begin(this->successors), std::end(this->successors),
-                       [x](const auto& s) { return (s.first.lock().get() == x); }),
-        std::end(this->successors));
+    this->remove(this->successors, x);
 }
 
 void CFGNode::removeSuccessor(const CFGNode* const x, bool isExecutable)
 {
-    this->successors.erase(std::remove_if(std::begin(this->successors), std::end(this->successors),
-                                          [x, isExecutable](const auto& s) {
-                                              return (s.first.lock().get() == x)
-                                                     && (s.second == isExecutable);
-                                          }),
-                           std::end(this->successors));
+    this->remove(this->successors, x, isExecutable);
 }
 
 void CFGNode::addPredecessor(CFGNode* x, bool isExecutable)
 {
-    auto sharedNode = std::dynamic_pointer_cast<CFGNode>(x->shared_from_this());
-    assert(sharedNode != nullptr);
-    this->predecessors.push_back({sharedNode, isExecutable});
+    this->add(this->predecessors, x, isExecutable);
 }
 
 void CFGNode::addPredecessor(std::unique_ptr<CFGNode>&& x, bool isExecutable)
 {
-    auto xPtr = x.get();
-    this->push_back(std::move(x));
-    this->addPredecessor(xPtr, isExecutable);
+    this->add(this->predecessors, std::move(x), isExecutable);
 }
 
 void CFGNode::setPredecessor(size_t index, CFGNode* x, bool isExecutable)
 {
-    auto sharedNode = std::dynamic_pointer_cast<CFGNode>(x->shared_from_this());
-    this->predecessors[index] = {sharedNode, isExecutable};
+    this->set(this->predecessors, index, x, isExecutable);
 }
 
 void CFGNode::setPredecessor(size_t index, std::unique_ptr<CFGNode>&& x, bool isExecutable)
 {
-    auto xPtr = x.get();
-    this->push_back(std::move(x));
-    this->setPredecessor(index, xPtr, isExecutable);
+    this->set(this->predecessors, index, std::move(x), isExecutable);
 }
 
 std::pair<CFGNode*, bool> CFGNode::getPredecessor(size_t x) const
 {
-    auto s = this->predecessors[x];
-    return {s.first.lock().get(), s.second};
+    return this->get(this->predecessors, x);
 }
 
 bool CFGNode::getPredecessorsEmpty() const
@@ -160,27 +125,17 @@ size_t CFGNode::getPredecessorSize() const
 
 void CFGNode::removePredecessor(size_t x)
 {
-    auto idx = std::begin(this->predecessors);
-    std::advance(idx, x);
-    this->predecessors.erase(idx);
+    this->remove(this->predecessors, x);
 }
 
 void CFGNode::removePredecessor(const CFGNode* const x)
 {
-    this->predecessors.erase(
-        std::remove_if(std::begin(this->predecessors), std::end(this->predecessors),
-                       [x](const auto& s) { return (s.first.lock().get() == x); }),
-        std::end(this->predecessors));
+    this->remove(this->predecessors, x);
 }
 
 void CFGNode::removePredecessor(const CFGNode* const x, bool isExecutable)
 {
-    this->predecessors.erase(
-        std::remove_if(std::begin(this->predecessors), std::end(this->predecessors),
-                       [x, isExecutable](const auto& s) {
-                           return (s.first.lock().get() == x) && (s.second == isExecutable);
-                       }),
-        std::end(this->predecessors));
+    this->remove(this->predecessors, x, isExecutable);
 }
 
 CFGNodeInfo* CFGNode::getCFGNodeInfo() const
@@ -193,4 +148,84 @@ CFGNodeInfo* CFGNode::getCFGNodeInfo() const
     }
 
     return nullptr;
+}
+
+void CFGNode::add(std::vector<std::pair<std::weak_ptr<CFGNode>, bool>>& vec, CFGNode* x,
+                  bool isExecutable)
+{
+    auto sharedNode = std::dynamic_pointer_cast<CFGNode>(x->shared_from_this());
+    assert(sharedNode != nullptr);
+    vec.push_back({sharedNode, isExecutable});
+}
+
+void CFGNode::add(std::vector<std::pair<std::weak_ptr<CFGNode>, bool>>& vec,
+                  std::unique_ptr<CFGNode>&& x, bool isExecutable)
+{
+    auto xPtr = x.get();
+    this->push_back(std::move(x));
+    this->add(vec, xPtr, isExecutable);
+}
+
+void CFGNode::set(std::vector<std::pair<std::weak_ptr<CFGNode>, bool>>& vec, size_t index,
+                  CFGNode* x, bool isExecutable)
+{
+    auto sharedNode = std::dynamic_pointer_cast<CFGNode>(x->shared_from_this());
+
+    if(index < vec.size())
+    {
+        vec[index] = {sharedNode, isExecutable};
+    }
+    else if(index == vec.size())
+    {
+        vec.push_back({sharedNode, isExecutable});
+    }
+    else
+    {
+        throw std::out_of_range(
+            "Attempt to set a CFGNode by an index outside the range of successors/predecessors.");
+    }
+}
+
+void CFGNode::set(std::vector<std::pair<std::weak_ptr<CFGNode>, bool>>& vec, size_t index,
+                  std::unique_ptr<CFGNode>&& x, bool isExecutable)
+{
+    auto xPtr = x.get();
+    this->push_back(std::move(x));
+    this->set(vec, index, xPtr, isExecutable);
+}
+
+std::pair<CFGNode*, bool> CFGNode::get(
+    const std::vector<std::pair<std::weak_ptr<CFGNode>, bool>>& vec, size_t x) const
+{
+    auto s = vec[x];
+    return {s.first.lock().get(), s.second};
+}
+
+void CFGNode::remove(std::vector<std::pair<std::weak_ptr<CFGNode>, bool>>& vec, size_t x)
+{
+    auto idx = std::begin(vec);
+    std::advance(idx, x);
+
+    if(idx != std::end(vec))
+    {
+        vec.erase(idx);
+    }
+}
+
+void CFGNode::remove(std::vector<std::pair<std::weak_ptr<CFGNode>, bool>>& vec,
+                     const CFGNode* const x)
+{
+    vec.erase(std::remove_if(std::begin(vec), std::end(vec),
+                             [x](const auto& s) { return (s.first.lock().get() == x); }),
+              std::end(vec));
+}
+
+void CFGNode::remove(std::vector<std::pair<std::weak_ptr<CFGNode>, bool>>& vec,
+                     const CFGNode* const x, bool isExecutable)
+{
+    vec.erase(std::remove_if(std::begin(vec), std::end(vec),
+                             [x, isExecutable](const auto& s) {
+                                 return (s.first.lock().get() == x) && (s.second == isExecutable);
+                             }),
+              std::end(vec));
 }
