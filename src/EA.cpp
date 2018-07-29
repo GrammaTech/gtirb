b524d271191a9112a2a808135a16430b531dcaa1
Nathan Weston <nweston@grammatech.com>
2018-07-29 12:12:38 -0400

    Improve conversions and arithmetic for EAs.
    Allow explicit conversion to and from all standard integer types.
    
    Support addition and subtraction with uint64_t.
    
    Fix += and -= operators to modify the EA rather than returning a new
    object.

--- src/EA.cpp
+++ src/EA.cpp
@@ -9,29 +9,56 @@ void EA::set(uint64_t x) { this->ea = x; }
 uint64_t EA::get() const { return this->ea; }
 
 EA::operator uint64_t() const { return this->ea; }
+EA::operator int64_t() const { return this->ea; }
+EA::operator uint32_t() const { return this->ea; }
+EA::operator int32_t() const { return this->ea; }
+EA::operator uint16_t() const { return this->ea; }
+EA::operator int16_t() const { return this->ea; }
+EA::operator uint8_t() const { return this->ea; }
+EA::operator int8_t() const { return this->ea; }
 
-EA& EA::operator=(EA x) {
+EA& EA::operator=(const EA x) {
   this->ea = x.ea;
   return *this;
 }
 
 bool EA::operator==(uint64_t x) const { return this->ea == x; }
 
-bool EA::operator==(const EA x) const { return this->ea == x.ea; }
+bool EA::operator==(EA x) const { return this->ea == x.ea; }
 
-bool EA::operator!=(const EA x) const { return this->ea != x.ea; }
+bool EA::operator!=(EA x) const { return this->ea != x.ea; }
 
-bool EA::operator>(const EA x) const { return this->ea > x.ea; }
+bool EA::operator>(EA x) const { return this->ea > x.ea; }
 
-bool EA::operator<(const EA x) const { return this->ea < x.ea; }
+bool EA::operator<(EA x) const { return this->ea < x.ea; }
 
-EA EA::operator+(const EA x) const { return EA(this->ea + x.ea); }
+EA EA::operator+(EA x) const { return EA(this->ea + x.ea); }
 
-EA EA::operator+=(const EA x) { return EA(this->ea += x.ea); }
+EA EA::operator+(uint64_t x) const { return EA(this->ea + x); }
 
-EA EA::operator-(const EA x) const { return EA(this->ea - x.ea); }
+EA& EA::operator+=(EA x) {
+  this->ea += x.ea;
+  return *this;
+}
 
-EA EA::operator-=(const EA x) { return EA(this->ea -= x.ea); }
+EA& EA::operator+=(uint64_t x) {
+  this->ea += x;
+  return *this;
+}
+
+EA EA::operator-(EA x) const { return EA(this->ea - x.ea); }
+
+EA EA::operator-(uint64_t x) const { return EA(this->ea - x); }
+
+EA& EA::operator-=(EA x) {
+  this->ea -= x.ea;
+  return *this;
+}
+
+EA& EA::operator-=(uint64_t x) {
+  this->ea -= x;
+  return *this;
+}
 
 EA::operator std::string() const {
   std::stringstream ss;
