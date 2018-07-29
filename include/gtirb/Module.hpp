aeeff87e8eddf74769c6df4562c9b7fe5c3487f1
Nathan Weston <nweston@grammatech.com>
2018-07-29 12:21:49 -0400

    Remove Enums.hpp.
    Move the enums to Modules.hpp where they are used.

--- include/gtirb/Module.hpp
+++ include/gtirb/Module.hpp
@@ -1,17 +1,13 @@
 #pragma once
 
+#include <proto/Module.pb.h>
 #include <boost/filesystem.hpp>
 #include <gtirb/CFG.hpp>
 #include <gtirb/EA.hpp>
-#include <gtirb/Enums.hpp>
 #include <gtirb/Node.hpp>
 #include <gtirb/NodeReference.hpp>
 #include <gtirb/SymbolSet.hpp>
 #include <gtirb/SymbolicOperandSet.hpp>
-
-namespace proto {
-class Module;
-}
 
 namespace gtirb {
 class AddrRanges;
@@ -23,6 +19,38 @@ struct Relocation;
 using RelocationSet = std::vector<Relocation>;
 class Section;
 using SectionSet = std::vector<Section>;
+
+///
+/// \enum FileFormat
+///
+enum class FileFormat : uint8_t {
+  Undefined = proto::Format_Undefined, /// Default value to indicates an uninitialized state.
+  COFF = proto::COFF,                  /// Common Object File Format (COFF)
+  ELF = proto::ELF, /// Executable and Linkable Format (ELF, formerly named Extensible Linking
+                    /// Format)
+  PE = proto::PE,   /// Microsoft Portable Executable (PE) format.
+  IdaProDb32 = proto::IdaProDb32, /// IDA Pro database file
+  IdaProDb64 = proto::IdaProDb64, /// IDA Pro database file
+  XCOFF = proto::XCOFF,           /// Non-COFF (files start with ANON_OBJECT_HEADER*)
+  MACHO = proto::MACHO            /// Mach object file format
+};
+
+///
+/// \enum ISAID
+///
+/// ISA ID
+///
+enum class ISAID : uint8_t {
+  Undefined = proto::ISA_Undefined, /// Default value to indicates an uninitialized state.
+  IA32 = proto::IA32,               /// Intel Architecture, 32-bit. Also known as i386.
+  PPC32 = proto::PPC32,             /// Performance Optimization With Enhanced RISC – Performance
+                                    /// Computing, 32-bit.
+  X64 = proto::X64, /// The generic name for the 64-bit extensions to both Intel's and AMD's
+                    /// 32-bit x86
+                    /// instruction set architecture (ISA).
+  ARM = proto::ARM, /// Advanced RISC Machine. also known as Acorn RISC Machine.
+  ValidButUnsupported = proto::ValidButUnsupported
+};
 
 ///
 /// \class Module
