602b34a6b67055201f3caebe181c70722e94c27d
Nathan Weston <nweston@grammatech.com>
2018-07-30 10:09:43 -0400

    Move utilities into main namespace.
    The nested namespace inhibits argument-dependent lookup and creates
    extra typing, for no particular benefit.

--- src/test/Utilities.test.cpp
+++ src/test/Utilities.test.cpp
@@ -10,9 +10,9 @@ using namespace gtirb;
 TEST(Unit_Utilities, ByteArray8To16) {
   const std::vector<uint8_t> input{2, 1, 1, 2, 0, 1};
   const std::vector<uint16_t> expectedOutput{258, 513, 256};
-  ASSERT_NO_THROW(gtirb::utilities::ByteArray8To16(input));
+  ASSERT_NO_THROW(ByteArray8To16(input));
 
-  const auto output = gtirb::utilities::ByteArray8To16(input);
+  const auto output = ByteArray8To16(input);
 
   EXPECT_EQ(expectedOutput[0], output[0]);
   EXPECT_EQ(expectedOutput[1], output[1]);
@@ -22,9 +22,9 @@ TEST(Unit_Utilities, ByteArray8To16) {
 TEST(Unit_Utilities, ByteArray8To16Swap) {
   const std::vector<uint8_t> input{2, 1, 1, 2, 0, 1};
   const std::vector<uint16_t> expectedOutput{513, 258, 1};
-  ASSERT_NO_THROW(gtirb::utilities::ByteArray8To16(input));
+  ASSERT_NO_THROW(ByteArray8To16(input));
 
-  const auto output = gtirb::utilities::ByteArray8To16(input, true);
+  const auto output = ByteArray8To16(input, true);
 
   EXPECT_EQ(expectedOutput[0], output[0]);
   EXPECT_EQ(expectedOutput[1], output[1]);
@@ -33,5 +33,5 @@ TEST(Unit_Utilities, ByteArray8To16Swap) {
 
 TEST(Unit_Utilities, ByteArray8To16Error) {
   const std::vector<uint8_t> input{2, 1, 1, 2, 0, 1, 0};
-  EXPECT_THROW(gtirb::utilities::ByteArray8To16(input), std::range_error);
+  EXPECT_THROW(ByteArray8To16(input), std::range_error);
 }
