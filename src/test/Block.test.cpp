2815252c1ccc58f329d6ca07392c8ac63023e3f5
Nathan Weston <nweston@grammatech.com>
2018-08-03 10:19:17 -0400

    Move decodeMode from Module to Block.
    On ARM, for example, this can change from one block to another.

--- src/test/Block.test.cpp
+++ src/test/Block.test.cpp
@@ -6,12 +6,18 @@ using namespace gtirb;
 
 TEST(Unit_Block, ctor) { EXPECT_NO_THROW(Block(EA(), EA())); }
 
+TEST(Unit_Block, getDecodeMode) {
+  Block b(EA(), 2, 3);
+  EXPECT_NO_THROW(b.getDecodeMode());
+  EXPECT_EQ(uint64_t{3}, b.getDecodeMode());
+}
+
 TEST(Unit_Block, protobufRoundTrip) {
   gtirb::Block result;
   proto::Block message;
 
   {
-    Block original{EA(1), 3};
+    Block original{EA(1), 3, 5};
     original.toProtobuf(&message);
   }
   // original has been destroyed, so UUIDs can be reused
@@ -19,4 +25,5 @@ TEST(Unit_Block, protobufRoundTrip) {
 
   EXPECT_EQ(result.getAddress(), EA(1));
   EXPECT_EQ(result.getSize(), 3);
+  EXPECT_EQ(result.getDecodeMode(), 5);
 }
