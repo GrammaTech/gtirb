7fccf187eb33e382423f04bd6f51b8edc9774f0d
Amy Gale <amyg@grammatech.com>
2018-09-03 15:16:18 -0400

    Doxygen skeletons for Module.hpp, Node.hpp, NodeRef.hpp, Table.hpp
    Full doxygen comment skeletons added to these four files.
    
    DOCFIXME variants to be addressed:
    
      - DOCFIXME[check]        : AG filled in this field, please verify.
    
      - DOCFIXME[check all]    : AG filled in multiple fields for this
                                 function (etc), please verify.
    
      - DOCFIXME[<other note>] : Consider adding indicated information to
                                 documentation for this field.
    
      - DOCFIXME               : Please fill in documentation for this field.
    
    Also update .gitignore for new ignorable items under doxy/

--- include/gtirb/Node.hpp
+++ include/gtirb/Node.hpp
@@ -16,62 +16,82 @@ using UUID = boost::uuids::uuid;
 ///
 /// \class Node
 ///
+/// \brief DOCFIXME
+///
 class GTIRB_EXPORT_API Node {
 public:
+
+  /// \brief Retrieve a node by its UUID.
   ///
-  /// Retrieve a node by its UUID.
-  ///
-  /// \return node with the given UUID, or nullptr if none exists.
+  /// \return The Node with the given UUID, or nullptr if none exists.
   static Node* getByUUID(UUID Uuid);
 
-  ///
+
+  /// \brief Default constructor.
   /// Automatically assigns the Node a UUID.
   ///
   Node();
 
   ///
-  /// Copy a node, assigning a new UUID to the copy.
+  /// \brief Copy constructor.
+  /// The copy is assigned a new UUID.
   ///
   explicit Node(const Node&);
 
-  ///
-  /// Nodes are not assignable due to UUIDs.
+
+  /// \brief Nodes are not assignable due to UUIDs.
   ///
   Node& operator=(const Node&) = delete;
 
-  ///
-  /// Move constructor. Clears the UUID of the moved node and updates
-  /// the UUID to node mapping.
+
+  /// \brief Move constructor.
+  /// Clears the UUID of the moved node and updates the UUID to node
+  /// mapping.
   ///
   Node(Node&& other) noexcept;
 
-  ///
-  /// Move assignment. Clears the UUID of the moved node and updates
-  /// the UUID to node mapping.
+
+  /// \brief Move assignment.
+  /// Clears the UUID of the moved node and updates the UUID to node
+  /// mapping.
   ///
   Node& operator=(Node&& other) noexcept;
 
-  ///
-  /// This will serve as a base class for other nodes.
+
+  /// \brief This will serve as a base class for other nodes.
   ///
   virtual ~Node() noexcept;
 
+
+  /// DOCFIXME[check all]
+  /// \brief Set the Universally Unique ID (UUID) for \c this to a
+  /// newly-generated value.
   ///
-  /// Generate and assign a new Universally Unique ID (UUID).
+  /// \return void
   ///
-  /// Though automatically assigned on construction, it can be manually set.
+  /// Each Node is automatically assigned a UUID on construction; this
+  /// method allows a different UUID to be assigned later.
+  /// DOCFIXME[what is the use case?]
   ///
   void setUUID();
 
+
+  /// DOCFIXME[check all]
+  /// \brief Set the Universally Unique ID (UUID) for \c this to the
+  /// specified value.
   ///
-  /// Manually assign Universally Unique ID (UUID).
+  /// \return void
   ///
-  /// Though automatically assigned on construction, it can be manually set.
+  /// Each Node is automatically assigned a UUID on construction; this
+  /// method allows a different UUID to be assigned later.
+  /// DOCFIXME[what is the use case? how is uniqueness enforced on X?] 
   ///
   void setUUID(UUID X);
 
+
+  /// \brief Get the Universally Unique ID (UUID) for \c this.
   ///
-  /// Retrieve the Node's Universally Unique ID (UUID).
+  /// \return The UUID.
   ///
   UUID getUUID() const { return Uuid; }
 
@@ -81,14 +101,25 @@ private:
   static std::map<UUID, Node*> UuidMap;
 };
 
+
+/// DOCFIXME[check all]
+/// \brief Return the text representation of a UUID.
 ///
-/// Return the text representation of a UUID.
+/// \param Uuid The UUID of interest.
 ///
+/// \return The text representation of \p Uuid.
+///  
 std::string uuidToString(const UUID& Uuid);
 
+/// DOCFIXME[check all]
+/// \brief Create a UUID from a text representation.
 ///
-/// Create UUID from text representation.
+/// \param Text The text representation. DOCFIXME[any constraints?]
 ///
+/// \return A new UUID corresponding to \p Text.
+///
+/// DOCFIXME[what if I call twice with the same Text?]
+///  
 UUID uuidFromString(const std::string& Text);
 
 } // namespace gtirb
