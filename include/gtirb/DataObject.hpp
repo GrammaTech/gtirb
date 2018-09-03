e6bbf32c48e36aaac0700316f720ae2ef94864b0
Amy Gale <amyg@grammatech.com>
2018-09-03 13:06:37 -0400

    Doxygen skeletons for Addr.hpp, DataObject.hpp, ImageByteMap.hpp, IR.hpp
    Full doxygen comment skeletons added to these three files.
    
    Files Export.hpp and gtirb.hpp have basenames within the alphabetical
    range D-I covered by this commit (EA->Addr being counted as E) but
    have no doxygen-commentable content.
    
    DOCFIXME variants to be addressed:
    
      - DOCFIXME[check]        : AG filled in this field, please verify.
    
      - DOCFIXME[check all]    : AG filled in multiple fields for this
                                 function (etc), please verify.
    
      - DOCFIXME[<other note>] : Consider adding indicated information to
                                 documentation for this field.
    
      - DOCFIXME               : Please fill in documentation for this field.

--- include/gtirb/DataObject.hpp
+++ include/gtirb/DataObject.hpp
@@ -13,36 +13,72 @@ namespace gtirb {
 ///
 /// \class DataObject
 ///
-/// Represents a data object, possibly symbolic. Does not directly store
-/// the data bytes, which are kept in the ImageByteMap.
+/// \brief Represents a data object, possibly symbolic.
+///
+/// Does not directly store the data bytes, which are kept in the
+/// \ref ImageByteMap.
 ///
 class GTIRB_EXPORT_API DataObject : public Node {
 public:
-  // Default constructor required for serialization.
+  /// \brief Default constructor required for serialization.
   DataObject() = default;
 
+
+
+  /// \brief Constructor DOCFIXME
+  ///
+  /// \param  Address_  DOCFIXME
+  /// \param  Size_     DOCFIXME
+  ///
   DataObject(Addr Address_, uint64_t Size_) : Address(Address_), Size(Size_) {}
 
-  ///
-  /// Copy constructor. Assigns a new UUID to the copy.
+
+  /// \brief Copy constructor.
+  /// Assigns a new UUID to the copy.
   ///
   explicit DataObject(const DataObject&) = default;
-  ///
-  /// Move constructor
+
+
+  /// \brief Move constructor.
   ///
   DataObject(DataObject&&) = default;
 
-  ///
-  /// Move assignment
+
+  /// \brief Move assignment.
   ///
   DataObject& operator=(DataObject&&) = default;
 
+
+  /// \brief Get the address of DOCFIXME.
+  ///
+  /// \return The address.
+  ///
   Addr getAddress() const { return Address; }
 
+  /// \brief Get the size of DOCFIXME.
+  ///
+  /// \return The size.
+  /// 
   uint64_t getSize() const { return Size; }
 
+
+  /// \brief DOCFIXME
   using MessageType = proto::DataObject;
+
+
+  /// \brief DOCFIXME
+  ///
+  /// \param Message DOCFIXME
+  ///
+  /// \return void
   void toProtobuf(MessageType* Message) const;
+
+
+  /// \brief DOCFIXME
+  ///
+  /// \param Message DOCFIXME
+  ///
+  /// \return void
   void fromProtobuf(const MessageType& Message);
 
 private:
