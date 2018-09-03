15af3802785fcf846585c7d84fcc7d8ab26d63a1
U-GRAMMATECH\amyg <amyg@amyg-w2.grammatech.com>
2018-09-03 11:42:37 -0400

    Doxygen skeletons for Block.hpp, ByteMap.hpp, CFG.hpp
    Full doxygen comment skeletons added to these three files.
    
    DOCFIXME variants to be addressed:
    
      - DOCFIXME[check]        : AG filled in this field, please verify.
    
      - DOCFIXME[check all]    : AG filled in multiple fields for this
                                 function (etc), please verify.
    
      - DOCFIXME[<other note>] : Consider adding indicated information to
                                 documentation for this field.
    
      - DOCFIXME[<other note>] : Please fill in documentation for this field.

--- include/gtirb/Block.hpp
+++ include/gtirb/Block.hpp
@@ -18,43 +18,86 @@ namespace gtirb {
 ///
 /// \class Block
 ///
-/// A basic block.
+/// \brief A basic block.
 ///
 class GTIRB_EXPORT_API Block : public Node {
 public:
-  // Default constructor
+  /// \brief Default constructor
   Block() = default;
 
-  ///
-  /// Copy constructor. Assigns a new UUID to the copy.
+
+  /// \brief Copy constructor.
+  /// Assigns a new UUID to the copy.
   ///
   explicit Block(const Block&) = default;
+
+
+  /// \brief Construct an empty block.
   ///
-  /// Construct an empty block
+  /// \param Address    The address of the first program point in the block. DOCFIXME[check]
   ///
+  /// \param Size       The size of the block, in DOCFIXME[bytes? program points?]
+  ///
+  /// \param DecodeMode DOCFIXME
+  ///
+  /// \return A new, empty block DOCFIXME
   Block(Addr Address, uint64_t Size, uint64_t DecodeMode = 0);
 
   ///
-  /// Move constructor
+  /// \brief Move constructor.
   ///
   Block(Block&&) = default;
 
-  ///
-  /// Move assignment
+
+  /// \brief Move assignment.
   ///
   Block& operator=(Block&&) = default;
 
-  ///
-  /// Defaulted trivial destructor.
+
+  /// \brief Defaulted trivial destructor.
   ///
   ~Block() override = default;
 
+
+  /// DOCFIXME[check all]
+  /// \brief Get the address from a \ref Block.
+  ///
+  /// \return The address.
+  ///
   Addr getAddress() const { return Address; }
+
+  /// DOCFIXME[check all]
+  /// \brief Get the size from a \ref Block.
+  ///
+  /// \return The size DOCFIXME[defined how?]
+  ///
   uint64_t getSize() const { return Size; }
+
+
+  /// \brief Get the decode mode from a \ref Block.
+  ///
+  /// \return The decode mode.
   uint64_t getDecodeMode() const { return DecodeMode; }
 
+
+  /// \brief DOCFIXME 
   using MessageType = proto::Block;
+
+  /// \brief DOCFIXME
+  ///
+  /// \param Message DOCFIXME
+  ///
+  /// \return DOCFIXME
+  ///
   void toProtobuf(MessageType* Message) const;
+
+
+  /// \brief DOCFIXME
+  ///
+  /// \param Message DOCFIXME
+  ///
+  /// \return DOCFIXME
+  ///
   void fromProtobuf(const MessageType& Message);
 
 private:
@@ -63,17 +106,33 @@ private:
   uint64_t DecodeMode{0};
 };
 
-///
+
 /// \class InstructionRef
 ///
-/// Describes the location of an instruction.
+/// \brief Describes the location of an instruction.
 ///
 struct GTIRB_EXPORT_API InstructionRef {
-  NodeRef<Block> BlockRef;
-  uint64_t Offset;
+  NodeRef<Block> BlockRef; ///< The block to which the instruction belongs. [DOCFIXME[check]
+  uint64_t Offset;         ///< The offset of the instruction in the block. DOCFIXME[in what? instructions? bytes?]
 
+
+  /// \brief DOCFIXME
   using MessageType = proto::InstructionRef;
+
+  /// \brief DOCFIXME
+  ///
+  /// \param message DOCFIXME
+  ///
+  /// \return DOCFIXME
+  ///
   void toProtobuf(MessageType* message) const;
+
+  /// \brief DOCFIXME
+  ///
+  /// \param message DOCFIXME
+  ///
+  /// \return DOCFIXME
+  ///
   void fromProtobuf(const MessageType& message);
 };
 
--- include/gtirb/ByteMap.hpp
+++ include/gtirb/ByteMap.hpp
@@ -19,36 +19,57 @@ namespace gtirb {
 ///
 /// \class ByteMap
 ///
-/// Holds the bytes of the loaded image of the binary.
+/// \brief Holds the bytes of the loaded image of the binary.
 ///
 class GTIRB_EXPORT_API ByteMap {
 public:
   ///
   /// Sets byte map at the given address.
   ///
-  /// \param  ea      The address to store the data.
-  /// \param  data    The data to store.
+  /// \param  A       The address to store the data.
+  /// \param  Data    The data to store.
   ///
+  /// \return  void
   void setData(Addr A, gsl::span<const std::byte> Data);
 
   ///
   /// Get data at the given address.
   ///
-  /// \param  ea       The starting address for the data.
-  /// \param  bytes   The number of bytes to read.
+  /// \param  A       The starting address for the data.
+  /// \param  Bytes   The number of bytes to read.
   ///
+  /// \return DOCFIXME
   std::vector<std::byte> getData(Addr A, size_t Bytes) const;
 
+  /// \return DOCFIXME
   using MessageType = proto::ByteMap;
+
+  /// \brief DOCFIXME
+  ///
+  /// \param Message DOCFIXME
+  ///
+  /// \return void
   void toProtobuf(MessageType* Message) const;
+
+  /// \brief DOCFIXME
+  ///
+  /// \param Message DOCFIXME
+  ///
+  /// \return void
   void fromProtobuf(const MessageType& Message);
 
   struct Region {
-    Addr Address;
-    std::vector<std::byte> Data;
+    Addr Address;                  ///< DOCFIXME
+    std::vector<std::byte> Data;   ///< DOCFIXME
 
+    /// \brief DOCFIXME
+    ///
+    /// \return DOCFIXME
     Addr getAddress() const { return this->Address; }
 
+    /// \brief DOCFIXME
+    ///
+    /// \return DOCFIXME
     uint64_t getSize() const { return this->Data.size(); }
   };
 
