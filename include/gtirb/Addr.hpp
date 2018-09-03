3e5cb24fc7fb4a0d1c61be79c2eef3206bd925bc
U-GRAMMATECH\amyg <amyg@amyg-w2.grammatech.com>
2018-09-03 13:06:37 -0400

    Doxygen skeletons for Addr.hpp, DataObject.hpp, ImageByteMap.hpp, IR.hpp
    Full doxygen comment skeletons added to these three files.
    
    Files Export.hpp and gtirb.hpp have basenames within the alphabetical
    range D-I covered by this commit (EA->Addr being counted as E) but
    have no doxygen-commentable content.
    
    DOCFIXME variants to be addressed:
    
      - DOCFIXME[check]        : AG filled in this field, please verify.
    
      - DOCFIXME[check all]    : AG filled in multiple fields for this
                                 function (etc), please verify.
    
      - DOCFIXME[<other note>] : Consider adding indicated information to
                                 documentation for this field.
    
      - DOCFIXME               : Please fill in documentation for this field.

--- include/gtirb/Addr.hpp
+++ include/gtirb/Addr.hpp
@@ -6,94 +6,211 @@
 #include <cstdint>
 
 namespace gtirb {
-/// A special class to store an Effective Address. It is a thin wrapper around
+/// \brief A special class to store an Effective Address.
+///
+/// It is a thin wrapper around
 /// a uint64_t for 64-bit address storage. An Addr cannot store a relative
 /// address as it cannot contain a negative number.
 class GTIRB_EXPORT_API Addr {
   uint64_t Address{0};
 
 public:
+  /// \brief Default constructor.
+  /// The default implementation provided by the compiler is used.
+  ///
   constexpr Addr() = default;
+
+
+  /// \brief Explicit conversion from \c uint64_t to \ref Addr.
+  ///
+  /// \param X The address.
+  ///
   constexpr explicit Addr(uint64_t X) : Address(X) {}
 
+
+  /// \brief Explicitly convert \ref Addr to \c uint64_t.
+  ///
+  /// \return An integer representation of the \ref Addr.
+  ///
   explicit operator uint64_t() const { return Address; }
 
-  // Increment and decrement Addr objects.
+
+  /// \brief Preincrement for \ref Addr.
+  ///
+  /// \return The incremented \ref Addr.
+  ///
+  /// DOCFIXME[what happens if represented address == max?]
   Addr& operator++() {
     ++Address;
     return *this;
   }
+
+
+  /// \brief Postincrement for \ref Addr.
+  ///
+  /// \return A new \ref Addr representing the incremented address.
+  ///
+  ///
+  /// DOCFIXME[what happens if represented address == max?]
   Addr operator++(int) {
     Addr R(*this);
     ++Address;
     return R;
   }
+
+
+  /// \brief Predecrement for \ref Addr.
+  ///
+  /// \return The decremented \ref Addr.
+  ///
+  ///
+  /// DOCFIXME[what happens if represented address == 0?]
   Addr& operator--() {
     --Address;
     return *this;
   }
+
+
+  /// \brief Postdecrement for \ref Addr.
+  /// 
+  /// \return A new \ref Addr representing the decremented address.
+  ///
+  /// DOCFIXME[what happens if represented address == 0?]
   Addr operator--(int) {
     Addr R(*this);
     --Address;
     return R;
   }
 
-  // Addr + integral should result in an Addr.
+
+  /// \brief Binary + operator for \ref Addr + integral offset.
+  /// 
+  /// \param A        The \ref Addr operand to +.
+  ///
+  /// \param Offset   The offset to add to \p A.
+  ///
+  /// \return A new \ref Addr representing \p A + \p Offset.
+  ///
   friend Addr operator+(const Addr& A, uint64_t Offset) {
     return Addr(A.Address + Offset);
   }
+
+
+  /// \brief Add-assign for \ref Addr.
+  /// 
+  /// \param Offset   The offset to add to the represented address.
+  ///
+  /// \return \c *this 
+  ///
+  /// DOCFIXME[what happens if represented address == max?]
   Addr& operator+=(uint64_t Offset) {
     Address += Offset;
     return *this;
   }
 
-  // Addr - integral should result in an Addr.
+
+  /// \brief Binary - operator for \ref Addr - integral offset.
+  /// 
+  /// \param A        The \ref Addr operand to -.
+  ///
+  /// \param Offset   The offset to subtract from \p A.
+  ///
+  /// \return A new \ref Addr representing  \p A - \p Offset.
+  ///
+  /// DOCFIXME[what happens if offset > the represented address?]
   friend Addr operator-(const Addr& A, uint64_t Offset) {
     return Addr(A.Address - Offset);
   }
+
+
+  /// \brief Subtract-assign for \ref Addr.
+  /// 
+  /// \param Offset   The offset to subtract from the represented address.
+  ///
+  /// \return \c *this 
+  ///
+  /// DOCFIXME[what happens if offset > the represented address?]
   Addr& operator-=(uint64_t Offset) {
     Address -= Offset;
     return *this;
   }
 
-  // Addr - Addr should result in a int64_t.
+  /// \brief Binary - operator for \ref Addr - \ref Addr.
+  /// 
+  /// \param A        The minuend.
+  ///
+  /// \param B        The subtrahend.
+  ///
+  /// \return         The \c int64_t difference \p A - \p B.
+  ///
+  /// DOCFIXME[what happens if offset > the represented address?]
   friend int64_t operator-(const Addr& A, const Addr& B) {
     return static_cast<int64_t>(A.Address - B.Address);
   }
 
-  // Equality and relational operators.
+  /// \brief Equality operator for \ref Addr.
   friend bool operator==(const Addr& LHS, const Addr& RHS) {
     return LHS.Address == RHS.Address;
   }
+
+  /// \brief Inquality operator for \ref Addr.
   friend bool operator!=(const Addr& LHS, const Addr& RHS) {
     return !operator==(LHS, RHS);
   }
+
+  /// \brief Less-than operator for \ref Addr.
   friend bool operator<(const Addr& LHS, const Addr& RHS) {
     return LHS.Address < RHS.Address;
   }
+
+  /// \brief Greater-than operator for \ref Addr.
   friend bool operator>(const Addr& LHS, const Addr& RHS) {
     return operator<(RHS, LHS);
   }
+
+  /// \brief Less-than-or-equal operator for \ref Addr.
   friend bool operator<=(const Addr& LHS, const Addr& RHS) {
     return !operator<(RHS, LHS);
   }
+
+  /// \brief Greater-than-or-equal operator for \ref Addr.
   friend bool operator>=(const Addr& LHS, const Addr& RHS) {
     return !operator<(LHS, RHS);
   }
 };
 
-/// Exclusive limit of object's address range.
+
+/// DOCFIXME[check all]
+/// \brief Exclusive upper limit of object's address range.
 ///
-/// Object can be any type which specifies a range of addresses via
-/// getAddress() and getSize() methods (e.g. DataObject).
+/// \param Object     DOCFIXME
+///
+/// \tparam T         Any type that specifies a range of 
+///                   addresses via getAddress() and getSize()
+///                   methods (e.g. DataObject).
+///
+/// \return An address (\ref Addr) A such that A-1 is in \p Object and
+/// A is not.
+///
 template <typename T> Addr addressLimit(const T& Object) {
   return Object.getAddress() + Object.getSize();
 }
 
-/// Does object contain a given Addr?
+
+/// DOCFIXME[check all]
+/// \brief Check: Does the specified object contain the specified address?
 ///
-/// Object can be any type which specifies a range of addresses via
-/// getAddress() and getSize() methods (e.g. DataObject).
+/// \param Object  The object of interest.
+///
+/// \param Ea      The address of interest.
+///
+/// \tparam T      Any type that specifies a range of 
+///                addresses via getAddress() and getSize()
+///                methods (e.g. DataObject).
+///
+/// \return \c true if \p Ea is in the address range of \p Object, \c
+/// false otherwise.
+///
 template <typename T> bool containsAddr(const T& Object, Addr Ea) {
   return Object.getAddress() <= Ea && addressLimit(Object) > Ea;
 }
--- include/gtirb/IR.hpp
+++ include/gtirb/IR.hpp
@@ -17,7 +17,7 @@ class Module;
 ///
 /// \class IR
 ///
-/// A complete IR consisting of Modules.
+/// \brief A complete IR consisting of Modules (\ref Module).
 ///
 /// \dot
 /// digraph example {
@@ -56,111 +56,181 @@ class Module;
 ///
 class GTIRB_EXPORT_API IR : public Node {
 public:
-  ///
-  /// Default constructor.
+
+  /// \brief Default constructor.
   ///
   IR();
 
-  ///
-  /// IR may not be copied due to Modules.
+
+  /// \brief IR may not be copied due to Modules. DOCFIXME[this is a bit mysterious]
   ///
   IR(const IR&) = delete;
 
-  ///
-  /// Move constructor
+
+  /// \brief Move constructor.
   ///
   IR(IR&&);
 
-  ///
-  /// Move assignment
+
+  /// \brief Move assignment.
   ///
   IR& operator=(IR&&);
 
   ///
-  /// Trivial virtual destructor.
+  /// \brief Trivial virtual destructor.
   ///
   ~IR() override;
 
+
+  /// DOCFIXME[check all]
+  ///
+  /// \brief Get all modules (\ref Module).
+  ///
+  /// \return All modules associated with \c this, as a \c
+  /// std::vector<Module>.
+  ///
   std::vector<Module>& getModules() { return Modules; }
+
+
+  /// DOCFIXME[check all]
+  ///
+  /// \brief Get all modules (\ref Module), as a const reference.
+  ///
+  /// \return All modules associated with \c this, as a 
+  /// <tt>const std::vector<Module></tt>.
+  ///
   const std::vector<Module>& getModules() const { return Modules; }
 
+
+  /// \brief Get all modules with the specified Preferred Effective Address.
   ///
-  /// Get all modules having the given Preferred address
+  /// \param X The EA of interest.
   ///
-  /// \sa Module::getPreferredAddr()
+  /// \return A <tt>std::vector<const Module*></tt> containing all
+  /// modules whose Preferred Effective Address is \p X.
+  ///
+  /// \sa Module::getPreferredEA()
   ///
   std::vector<const Module*> getModulesWithPreferredAddr(Addr X) const;
 
+
+  /// \brief Get all modules containing the specified address.
   ///
-  /// Get all modules continaing the given address.
+  /// \param X The address of interest.
   ///
-  /// The test is [lower bound inclusive, upper bound exclusive)
+  /// \return A <tt>std::vector<const Module*></tt> containing all
+  /// modules with (minimum,maximum) effective addresses such that
+  /// minimum <= X < maximum.
   ///
-  /// \sa Module::getAddrMinMax()
+  /// \sa Module::getEAMinMax()
   ///
   std::vector<const Module*> getModulesContainingAddr(Addr X) const;
 
+
+  /// \brief Serialize to an output stream.
   ///
-  /// Serialize IR to an output stream.
+  /// \param Out The output stream.
+  ///
+  /// \return void
   ///
   void save(std::ostream& Out) const;
 
+
+  /// \brief Deserialize from an input stream.
   ///
-  /// Deserialize IR from an input stream.
+  /// \param In  The input stream.
+  ///
+  /// \return void
   ///
   void load(std::istream& In);
 
+
+  /// \brief DOCFIXME
   using MessageType = proto::IR;
+
+  ///
+  /// \brief DOCFIXME
+  ///
+  /// \param Message DOCFIXME
+  ///
+  /// \return void
+  ///
   void toProtobuf(MessageType* Message) const;
+
+  ///
+  /// \brief DOCFIXME
+  ///
+  /// \param Message DOCFIXME
+  ///
+  /// \return void
+  ///
   void fromProtobuf(const MessageType& Message);
 
-  // ----------------------------------------------------------------------------------------
+  // ----------------------------------------------------------------------
   // Table Properties
 
-  ///
-  /// Add a new table, transferring ownership.
+
+  /// \brief Add a new table, transferring ownership.
   /// The table can be populated from anywhere.
   ///
-  /// \param name     The name to assign to the table so it can be found later.
+  /// \param Name     The name to assign to the table so it can be found later.
   /// \param X        The table itself.
+  /// \return void
   ///
   void addTable(std::string Name, Table&& X);
 
-  ///
-  /// Get a table by name.
+
+  /// \brief Get a table by name.
   ///
   /// \param  X   The name of the table to search for.
-  /// \return     A non-owning pointer to the table if found, or nullptr.
+  ///
+  /// \return     A non-owning pointer to the table if found,
+  ///             \c nullptr otherwise.
   ///
   const gtirb::Table* getTable(const std::string& X) const;
+
+
+  /// \brief Get a table by name.
+  /// DOCFIXME[distinction  from previous]
+  ///
+  /// \param  X   The name of the table to search for.
+  ///
+  /// \return     A non-owning pointer to the table if found,
+  ///             \c nullptr otherwise.
+  ///
   gtirb::Table* getTable(const std::string& X);
 
-  ///
-  /// Remove a table by name.
+
+  /// \brief Remove a table by name.
   ///
   /// This will invalidate any pointers that may have been held externally.
   ///
   /// \param  X   The name of the table to search for.
-  /// \return     True on success.
+  /// \return     \c true on success, \c false otherwise.
   ///
   bool removeTable(const std::string& X);
 
-  ///
-  /// Get the total number of tables at this Node.
+
+  /// \brief Get the total number of tables at this Node.
+  /// DOCFIXME[what Node?]
   ///
   /// \return     The total number of tables this node owns.
   ///
   size_t getTableSize() const { return Tables.size(); }
 
+
+  /// \brief Check: Is the number of tables at this Node zero?
+  /// DOCFIXME[what Node?]
   ///
-  /// Test to see if the number of tables at this Node is zero.
-  ///
-  /// \return     True if this node does not own any tables.
+  /// \return \c true if this node does not own any tables, \c false
+  /// if it owns one or more tables.
   ///
   bool getTablesEmpty() const { return Tables.empty(); }
 
+
+  /// \brief Clear all locally owned tables.
   ///
-  /// Clear all locally owned tables.
+  /// \return void
   ///
   void clearTables() { Tables.clear(); }
 
