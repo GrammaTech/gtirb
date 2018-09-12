55aad0e5988607bba5497b0e9ffa4780b2d48548
aballman <aballman@grammatech.com>
2018-09-12 09:40:12 -0400

    Tidy up the macros in Export.hpp.
    Improved the way we determine which macro to use so that it supports a
    wider range of compilers.
    
    Changed the documentation to hopefully make it more clear what
    information is important in the file and what is boilerplate.

--- include/gtirb/Export.hpp
+++ include/gtirb/Export.hpp
@@ -1,35 +1,31 @@
 #ifndef GTIRB_EXPORT_H
 #define GTIRB_EXPORT_H
 
-// DOXFIXME[What does this comment belong to? At the moment it's being
-// attached to namespace gtirb which seems definitely wrong]
-///
-/// Defined by the build system (CMake or SCons).
-/// This should only be defined by the build system which generates the GT-IRB
-/// library.  Users of the library should NOT define this.
-///
+#ifndef __has_declspec_attribute
+#define __has_declspec_attribute(x) 0
+#endif
 
-// DOXFIXME[What does this comment belong to? At the moment it's being
-// attached to namespace gtirb which seems definitely wrong]
-///
-/// This controls the visibility of exported symbols (i.e. classes) in Windows
-/// DLL's and Linux Shared Objects.
-///
+#ifndef __has_attribute
+#define __has_attribute(x) 0
+#endif
 
-///
-/// \namespace gtirb
-///
-/// The namespace for the GT-IRB library.
-///
-
-#ifdef _WIN32
-#if defined GTIRB_gtirb_EXPORTS
+/// \def GTIRB_EXPORT_API
+/// \brief This macro controls the visibility of exported symbols (i.e. classes)
+/// in shared libraries. When producing the library, selects symbols to export,
+/// and when consuming the library, selects symbols to import.
+#if defined(_MSC_VER) || __has_declspec_attribute(dllexport)
+// Defined by the build system (CMake or SCons). This should only be defined by
+// the build system which generates the GT-IRB library. Users of the library
+// should NOT define this.
+#ifdef GTIRB_gtirb_EXPORTS
 #define GTIRB_EXPORT_API _declspec(dllexport)
 #else
 #define GTIRB_EXPORT_API _declspec(dllimport)
-#endif
-#else
+#endif // GTIRB_gtirb_EXPORTS
+#elif defined(__GNUC__) || __has_attribute(visibility)
 #define GTIRB_EXPORT_API __attribute__((visibility("default")))
+#else
+#define GTIRB_EXPORT_API
 #endif
 
 #endif // GTIRB_EXPORT_H
