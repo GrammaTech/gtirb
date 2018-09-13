2f2b142b177dcc06ef622085172b5701decfbe52
aballman <aballman@grammatech.com>
2018-09-13 07:39:59 -0400

    Initial support for visit() on Symbols.

--- include/gtirb/Symbol.hpp
+++ include/gtirb/Symbol.hpp
@@ -20,6 +20,8 @@
 #include <gtirb/DataObject.hpp>
 #include <gtirb/Node.hpp>
 #include <proto/Symbol.pb.h>
+#include <functional>
+#include <optional>
 #include <type_traits>
 
 namespace gtirb {
@@ -35,11 +37,122 @@ template <> struct NodeTraits<Block> {
   static constexpr bool IsReferent = true;
 };
 
+namespace details {
+template <typename... Ts> struct TypeList {};
+}
+
 /// \class Symbol
 ///
 /// \brief DOCFIXME
 class GTIRB_EXPORT_API Symbol : public Node {
+  // SFINAE overload handling a Callable with a void return type.
+  template <typename Callable, typename Ty>
+  auto visit_impl_help(Callable&& Visitor) -> std::enable_if_t<
+      std::is_void_v<decltype(std::invoke(Visitor, (Ty*)0))>> {
+    static_assert(std::is_invocable_v<Callable, Ty*>,
+                  "Visitor must contain an overloaded function call operator "
+                  "for each of the types in supported_types");
+    if (Ty* Obj = dyn_cast_or_null<Ty>(this->Referent)) {
+      std::invoke(Visitor, Obj);
+    }
+  }
+
+  // SFINAE overload handling a Callable with a non-void return type.
+  template <typename Callable, typename Ty>
+  auto visit_impl_help(Callable&& Visitor) -> std::enable_if_t<
+      !std::is_void_v<decltype(std::invoke(Visitor, (Ty*)0))>,
+      std::optional<decltype(std::invoke(Visitor, (Ty*)0))>> {
+    static_assert(std::is_invocable_v<Callable, Ty*>,
+                  "Visitor must contain an overloaded function call operator "
+                  "for each of the types in supported_types");
+    if (Ty* Obj = dyn_cast_or_null<Ty>(this->Referent)) {
+      return std::invoke(Visitor, Obj);
+    }
+    return std::nullopt;
+  }
+
+  // Helper type traits used to determine whether all of the Callable object's
+  // return types agree. Note that the return types do not need to be the same,
+  // but do need to be implicitly convertible to the same common type. e.g.,
+  // this class is a valid Callable.
+  //   struct Visitor {
+  //     int operator()(Block*);
+  //     long operator()(DataObject*);
+  //  };
+  template <typename AlwaysVoid, typename Callable,
+            template <typename...> typename TypeList, typename... Types>
+  struct has_common_type_impl : std::false_type {};
+
+  template <typename Callable, template <typename...> typename TypeList,
+            typename... Types>
+  struct has_common_type_impl<std::void_t<std::common_type_t<
+                                  std::invoke_result_t<Callable, Types*>...>>,
+                              Callable, TypeList, Types...> : std::true_type {};
+
+  template <typename Callable, template <typename...> typename TypeList,
+            typename... Types>
+  static constexpr bool has_common_type_v =
+      has_common_type_impl<void, Callable, TypeList, Types...>::value;
+
+  // Helper function to unpack all of the types in the type list and attempt to
+  // visit Callable once per type. Verifies that the Callable objects all share
+  // a compatible return type.
+  template <typename Callable, template <typename...> typename TypeList,
+            typename... Types>
+  auto visit_impl(Callable&& Visitor, TypeList<Types...>) {
+    // If this assertion fails, the return values from the Callable object are
+    // not compatible enough. This can happen if they return incompatible types
+    // or if there is not an overload for each referent type.
+    static_assert(has_common_type_v<Callable, TypeList, Types...>,
+                  "incompatible return types for the Callable object");
+    // Instantiate a call to the Visitor once for each of the listed types, but
+    // only issue the call at runtime if the Referent can be dynamically cast to
+    // the given type. In this way, the Visitor needs to be able to handle any
+    // of the supported types, but will only be called once for the concrete
+    // type of the Referent.
+    //
+    // If you get an error about there being no matching overloaded function for
+    // the call to visit_impl_help, that is most likely because there are one
+    // or more overloads missing for each referent type.
+    return (...,
+            visit_impl_help<Callable, Types>(std::forward<Callable>(Visitor)));
+  }
+
 public:
+  /// \brief The list of supported referent types.
+  using supported_referent_types = details::TypeList<Block, DataObject>;
+
+  /// \brief Visits the symbol's referent, if one is present, by concrete
+  /// referent type. For example:
+  ///
+  /// \code
+  /// struct Visitor {
+  ///   int operator()(Block*) { return 0; }
+  ///   long operator()(DataObject*) { return 1; }
+  /// };
+  ///
+  /// Context Ctx;
+  /// Symbol* SymB = Symbol::Create(Ctx, Addr(0), "", Block::Create(Ctx));
+  /// Symbol* SymD = Symbol::Create(Ctx, Addr(0), "", DataObject::Create(Ctx));
+  /// Symbol* SymN = Symbol::Create(Ctx);
+  ///
+  /// SymB->visit(Visitor{}); // Will call Visitor::operator()(Block*);
+  /// SymD->visit(Visitor{}); // Will call Visitor::operator()(DatObject*);
+  /// SymN->visit(Visitor{}); // Will not call either overload
+  /// \endcode
+  ///
+  /// \tparam Callable  A callable function type. This type must be able to be
+  /// called with a pointer to all of the types listed in \ref
+  /// supported_referent_types. All overloaded functions must have a common,
+  /// compatible return type.
+  ///
+  /// \param Visitor  A callable object that will be called with a nonnull
+  /// symbol referent.
+  template <typename Callable> auto visit(Callable&& Visitor) {
+    return visit_impl(std::forward<Callable>(Visitor),
+                      std::decay_t<supported_referent_types>{});
+  }
+
   /// \enum StorageKind
   ///
   /// \brief DOCFIXME
