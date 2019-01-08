f672462e0fff6e704db4f424d27034f4973484bd
Amy Gale <amyg@grammatech.com>
2019-01-07 20:30:56 -0500

    Tune cfg-paths examples (py,java)
    Also improve Doxyfile robustness.

--- doc/examples/cfg-paths.py
+++ doc/examples/cfg-paths.py
@@ -3,7 +3,6 @@
 # An example program which opens an IR and prints every control-flow
 # path from some basic block to another basic block.
 #
-
 # To run this example, do the following.
 #
 # 1. Install the protobuf compiler (protoc) from
@@ -40,24 +39,33 @@ import sys
 
 from IR_pb2 import IR
 
+# Avoid duplicated effort by caching the uuids of Blocks from
+# which we have know the target is unreachable.
+unreachability_cache = set([])
+
 def print_paths(blocks, edges, target_id, vertex_id, visited=set(), path=[]):
     """Depth-first search of a graph, printing all paths between two vertices."""
-
+    rv = 0
     visited.add(vertex_id)
     path.append(blocks[vertex_id].address)
 
     # At target, print the path
     if vertex_id == target_id:
         print(', '.join('0x%.8x' % s for s in path))
+        rv= 1
+
+    # Otherwise, check all outgoing edges from this vertex
     else:
-        # Otherwise, check all outgoing edges from this vertex
         for v in edges[vertex_id]:
-            if v not in visited:
-                print_paths(blocks, edges, target_id, v, visited, path)
+            if v not in visited and v not in unreachability_cache:
+                rv += print_paths(blocks, edges, target_id, v, visited, path)
 
     path.pop()
     # Unmark the node so it can be visited again in other paths
     visited.discard(vertex_id)
+    if rv==0:
+        unreachability_cache.add(vertex_id)
+    return rv
 
 def auto_int(x):
     return int(x, 0)
@@ -69,29 +77,51 @@ parser.add_argument('source', type=auto_int, help='Address of the source block')
 parser.add_argument('target', type=auto_int, help='Address of the target block')
 args = parser.parse_args()
 
+def find_block(addr,mod):
+    return next((b for b in mod.cfg.blocks if b.address == addr), None)
+
 ir = IR()
 ir.ParseFromString(args.ir.read())
 
-m = ir.modules[0]
 
-def find_block(addr):
-    return next((b for b in m.cfg.blocks if b.address == addr), None)
-source_block = find_block(args.source)
-target_block = find_block(args.target)
+oneblock_fmt = "{0}-block {1:08X} is located in a module that does not contain  {2}-block {3:08X}.\nThe {2}-block may be in a different module, or may not exist."
 
-if source_block is None:
-    print('No block at source address 0x%.8x' % args.source, file=sys.stderr)
+found = False
+# Check one module at a time (the GTIRB CFGs do not have edges across
+# module boundaries, so if the specified blocks are in different
+# modules there cannot be any paths between them).
+for m in ir.modules:
+    source_block = find_block(args.source, m)
+    target_block = find_block(args.target, m)
+
+    # neither block is in current module: go on to next
+    if (source_block is None) and (target_block is None):
+        continue
+
+    # only one block is in current module: there cannot be any paths.
+    if source_block is None:
+        print(oneblock_fmt.format('target', args.target, 'source', args.source))
+        sys.exit(1)
+    if target_block is None:
+        print(oneblock_fmt.format('source', args.source, 'target', args.target))
+        sys.exit(1)
+
+    # if execution reaches this point, both blocks are in current module
+    found=True
+
+    # Convert the CFG to a simple adjacency list
+    blocks = { b.uuid: b for b in m.cfg.blocks }
+    edges = collections.defaultdict(list)
+    for e in m.cfg.edges:
+        edges[e.source_uuid].append(e.target_uuid)
+
+    print("Paths from {0:08X} to {1:08X}".format(source_block.address,
+                                                 target_block.address))
+    num = print_paths(blocks, edges, target_block.uuid, source_block.uuid)
+
+    print(num, "paths found.")
+    break
+
+if not found:
+    print("Neither block was found:\n source {0:08X}\n target {1:08X}".format(args.source, args.target))
     sys.exit(1)
-
-if target_block is None:
-    print('No block at target address 0x%.8x' % args.target, file=sys.stderr)
-    sys.exit(1)
-
-# Convert the CFG to a simple adjacency list
-blocks = { b.uuid: b for b in m.cfg.blocks }
-edges = collections.defaultdict(list)
-for e in m.cfg.edges:
-    edges[e.source_uuid].append(e.target_uuid)
-
-print('Paths from %.8x to %.8x:' % (source_block.address, target_block.address))
-print_paths(blocks, edges, target_block.uuid, source_block.uuid)
---
+++ doc/examples/cfgpaths.java
@@ -0,0 +1,230 @@
+// An example program which opens an IR and prints every control-flow
+// path from some basic block to another basic block.
+//
+// To run this example, do the following.
+//
+// 1. Install the protobuf compiler (protoc) from
+//    https://github.com/protocolbuffers/protobuf/releases (if you have
+//    not already done so).
+//
+// 2. Download the protobuf Java runtime from
+//    https://mvnrepository.com/artifact/com.google.protobuf/protobuf-java
+//    and save it somewhere suitable.
+//
+// 3. Generate Java message definitions.
+//
+//    $ mkdir -p java $ for f in src/proto/*.proto; do
+//         protoc -Isrc/proto --java_out=java $f
+//      done
+//
+//    This will create a subdirectory java/proto/, containing a
+//    number of files with names of the form <bn>OuterClass.java:
+//    one for each <bn>.proto in src/proto/.
+//
+// 4. Compile the Java message definitions, making sure the protobuf
+//    Java runtime .jar file is in your CLASSPATH.
+//
+//    $ mkdir -p java/classfiles
+//    $ CLASSPATH=<path/to/protobuf_jar> \
+//         javac -d java/classfiles java/proto/*.java
+//
+// 5. Compile the datasymbols class defined in this file, making sure
+//    your CLASSPATH contains both the Java runtime .jar file and the
+//    compiled Java message definition classes.
+//    (Note that the path separator is OS-dependent.)
+//
+//    $ CLASSPATH=<path/to/protobuf_jar>:./java/classfiles/ \
+//         javac doc/examples/cfgpaths.java
+//
+// 6. Execute the following command to run the program on the
+//    serialized GTIRB data located at <path-to-ir> printing every
+//    control-flow path between the block with address <source-addr>
+//    and the block with address <target-addr>.
+//
+//    $  CLASSPATH=<path/to/protobuf_jar>:./java/classfiles/:doc/examples/ \
+//          java cfgpaths <path-to-ir>  <source-addr> <target-addr>
+
+import com.google.protobuf.ByteString;
+import proto.IROuterClass.IR;
+import proto.BlockOuterClass.Block;
+import proto.CFGOuterClass.CFG;
+import proto.CFGOuterClass.Edge;
+import proto.ModuleOuterClass.Module;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.Long;
+
+class cfgpaths {
+
+    // Avoid duplicated effort by caching the uuids of Blocks from
+    // which we have the target is unreachable.
+    static Set<ByteString> unreachability_cache = new HashSet<ByteString>();
+
+    // Print one path.
+    static void printPath(Stack<Long> path){
+	Set<Long> pathset = new HashSet<Long>();
+	pathset.addAll(path);
+	//	assert path.size() == pathset.size();
+	for (Long addr : path){
+	    System.out.print(String.format("0x%08X ", addr));
+	}
+	System.out.println();
+    }
+
+    // Use depth-first search to print all paths from the Block with
+    // uuid src to targ to the Block with uuid targ.
+    static int printPathsRec(ByteString src,
+			     ByteString targ,
+			     Map<ByteString,Block> blocks,
+			     Map<ByteString,Set<ByteString>> edges,
+			     Set<ByteString> visited,
+			     Stack<Long> path){
+
+	int printed=0;
+	path.push(blocks.get(src).getAddress());
+	visited.add(src);
+	if (src.equals(targ)){
+	    printPath(path);
+	    printed = 1;
+	} else{
+	    for (ByteString next : edges.get(src)) {
+		if ( !visited.contains(next)
+		     && !unreachability_cache.contains(next)){
+		    printed += printPathsRec(next,
+					     targ,
+					     blocks,
+					     edges,
+					     visited,
+					     path);
+		}
+	    }
+	}
+	visited.remove(src);
+	path.pop();
+	if (printed==0){
+	    unreachability_cache.add(src);
+	}
+	return printed;
+    }
+
+    // Print all paths from source to target in cfg.
+    static void printPaths(Block source, Block target, CFG cfg){
+	Map<ByteString,Block> blocks = new HashMap<ByteString,Block>();
+	Map<ByteString,Set<ByteString>> edges = new HashMap<ByteString,Set<ByteString>>();
+	Set<ByteString> visited = new HashSet<ByteString>();
+
+	for (Block b : cfg.getBlocksList()){
+	    blocks.put(b.getUuid(), b);
+	    edges.put(b.getUuid(), new HashSet<ByteString>());
+	}
+
+	for (Edge e : cfg.getEdgesList()){
+	    edges.get(e.getSourceUuid()).add(e.getTargetUuid());
+	}
+
+	int numpaths = printPathsRec(source.getUuid(),
+				     target.getUuid(),
+				     blocks,
+				     edges,
+				     new HashSet<ByteString>(),
+				     new Stack<Long>());
+	System.out.println(numpaths + " paths found.");
+    }
+
+
+    static Block findBlockByAddr(Long addr, CFG cfg){
+	for (Block b : cfg.getBlocksList()){
+	    if (b.getAddress() == addr){
+		return b;
+	    }
+	}
+	return Block.getDefaultInstance();
+    }
+
+    public static void main(String[] args) {
+
+        IR ir = IR.getDefaultInstance();
+	Block defaultblock = Block.getDefaultInstance();
+	Block from_block = defaultblock;
+	Block to_block = defaultblock;
+	CFG cfg = CFG.getDefaultInstance();
+	boolean blocks_found = false;
+	Long from_addr =-1L;
+	Long to_addr =-1L;
+
+
+        if (args.length < 3){
+            System.err.println("Requires three arguments: <gtirb-file> <from-address> <to-address> ");
+            System.exit(-1);
+        }
+
+        // Read serialized GTIRB data from the specified file.
+        try {
+            ir = IR.parseFrom(new FileInputStream(args[0]));
+        } catch (FileNotFoundException fe) {
+            System.err.println("File not found: " + args[0]);
+            System.exit(-1);
+        } catch (IOException ie) {
+            System.err.println("Problem reading file: " + args[0]);
+            System.exit(-1);
+        }
+
+	try {
+	    from_addr = Long.decode(args[1]);
+	    to_addr = Long.decode(args[2]);
+	} catch (NumberFormatException nfe) {
+            System.err.println("Specify block addresses as numbers in decimal, hexadecimal, or octal format.");
+            System.exit(-1);
+        }
+
+	// Check one module at a time (the GTIRB CFGs do not have
+	// edges across module boundaries, so if the specified blocks
+	// are in different modules there cannot be any paths between
+	// them).
+        for (Module m : ir.getModulesList()) {
+	    cfg = m.getCfg();
+	    from_block = findBlockByAddr(from_addr, cfg);
+	    to_block = findBlockByAddr(to_addr, cfg);
+
+	    // Neither block is in this module: go on to next module.
+	    if (from_block==defaultblock && to_block==defaultblock){
+		System.out.println("Blocks not found in module " + m.getName());
+		continue;
+	    }
+
+	    // One block is in this module and the other isn't: error.
+	    if (to_block==defaultblock){
+		System.err.println(String.format("The block at from-address 0x%08X is located in a module that does not contain a block at to-address 0x%08X.  The block at 0x%08X may be in a different module, or may not exist.",
+						 from_addr,
+						 to_addr,
+						 to_addr));
+		System.exit(-1);
+	    }
+	    if (from_block==defaultblock){
+		System.err.println(String.format("The block at  to-address 0x%08X is located in a module that does not contain a block at from-address 0x%08X. The block at 0x%08X may be in a different module, or may not exist.",
+						 to_addr,
+						 from_addr,
+						 from_addr));
+		System.exit(-1);
+	    }
+
+	    // At this point, both to_block and from_block must be
+	    // present in this module.
+	    blocks_found = true;
+	    System.out.println("Blocks found in module " + m.getName());
+	    printPaths(from_block, to_block, cfg);
+	    break;
+	}
+
+	// If never found either block, notify user.
+	if (!blocks_found){
+	    System.err.println(String.format("No blocks found at either of the specified addresses: \n to-address: 0x%08X \n from-address: 0x%08X",
+					     to_addr,
+					     from_addr));
+	    System.exit(-1);
+	}
+    }
+}
