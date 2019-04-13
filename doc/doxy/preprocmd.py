93a63123866d54522bc15e3d0d569446a4d2e682
Eric Schulte <eschulte@grammatech.com>
2019-04-12 21:50:36 -0400

    Remove documentation dependency on Python2

--- doc/doxy/preprocmd.py
+++ doc/doxy/preprocmd.py
@@ -1,26 +1,22 @@
 # doxygen is annoying about certain .md things so we have to
 # preprocess before doxygenating
-
 import re
 import sys
-
-
-(infile, outfile) = sys.argv[1:3]
+from functools import reduce
 
 def anchor_heading(headtxt, anchortxt):
     return '{0}<a name="{1}" id="{1}"></a>\n\n'.format(
-         headtxt,
-         reduce (lambda intxt,(oldtxt,newtxt): intxt.replace(oldtxt,newtxt),
-                  [anchortxt.lower(),
-                   (', ',''), ('/',''), (' ', '-')
-                   ]
-                  ))
+        headtxt,
+        reduce (lambda intxt,rep: intxt.replace(rep[0],rep[1]),
+                [anchortxt.lower(),
+                 (', ',''), ('/',''), (' ', '-')]))
 
+(infile, outfile) = sys.argv[1:3]
 with open(infile,'r') as infh:
     contents = infh.read()
     
     contents = reduce(
-        lambda intxt, (inre,replacement): re.sub(inre,replacement,intxt),
+        lambda intxt, rep: re.sub(rep[0],rep[1],intxt),
         [contents,
          
          # doxygen doesn't make heading anchors like it should
@@ -29,8 +25,7 @@ with open(infile,'r') as infh:
 
          (r'(?:(?<=\n)|^)(\S(.*))\n(?:(\=\=\=+|\-\-\-+)\n)',
           lambda m:anchor_heading(m.group(0), m.group(1))), 
-
-     
+         
          # doxygen is specifically annoying about fenced code, which
          # only occurs in README.md. 
          (r'\n( +)```bash(\n(.*\n)*?)\1```',
@@ -41,20 +36,9 @@ with open(infile,'r') as infh:
          (r'\[([^]\n]*)\]\s*\(doc/examples/(.*?)\)',
           r'\\ref \2 "\1"'), 
 
-         # C++ has to be converted to doxygen \code ...\endcode to get
-         # linking
+         # C++ has to be converted to doxygen \code ...\endcode to get linking
          (r'```c\+\+(\n(.*\n)*?)```',
-          r'\\code{.cpp}\1\\endcode')
-         ]
-        )
-
-   # print contents
-
+          r'\\code{.cpp}\1\\endcode')])
 
     with open(outfile,'w') as outfh:
         outfh.write(contents)
-
-print "md preprocessing done\n\n"
-
-
-
