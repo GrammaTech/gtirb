3bfa5c0fd9da76db881e23fe8222e301d8314bbf
Abhishek Vasisht Bhaskar <abhaskar@grammatech.com>
2019-05-03 13:28:43 -0400

    Add a simple testing framework. `make pycheck` to run tests

---
+++ python/tests/test_ir_open.py
@@ -0,0 +1,19 @@
+import unittest
+import gtirb
+import IR_pb2
+
+class MainTest(unittest.TestCase):
+    def test_ir_open(self):
+        import os
+        test_path = os.path.dirname(os.path.realpath(__file__))
+
+        with open(os.path.join(test_path, 'test1.gtir'), 'rb') as f:
+            _ir = IR_pb2.IR()
+            _ir.ParseFromString(f.read()) 
+
+            factory = gtirb.Factory()
+            ir = gtirb.IR.fromProtobuf(factory, _ir)
+            self.assertTrue(ir is not None)
+
+if __name__ == '__main__':
+    unittest.main()
