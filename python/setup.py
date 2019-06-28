c033e75f534d5f92e1ace7ac0b88943d9db11033
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2019-06-28 17:00:07 -0400

    fix python tests

--- python/setup.py
+++ python/setup.py
@@ -5,20 +5,21 @@ def gtirb_test_suite():
     test_loader = unittest.TestLoader()
     test_suite = test_loader.discover('tests', pattern='test_*.py')
     return test_suite
-            
-setup(
-    name="gtirb",
-    version="0.0.1",
-    author="abhaskar",
-    author_email="abhaskar@grammatech.com",
-    description="The gtirb package",
-    package_data={'gtirb': ['gtirb/*.py']},
-    packages=find_packages(),
-    test_suite='setup.gtirb_test_suite',
-    install_requires=[
-        'protobuf',
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3"
-    ],
-)
+
+if __name__ == '__main__':
+    setup(
+        name="gtirb",
+        version="0.0.1",
+        author="abhaskar",
+        author_email="abhaskar@grammatech.com",
+        description="The gtirb package",
+        package_data={'gtirb': ['gtirb/*.py']},
+        packages=find_packages(),
+        test_suite='setup.gtirb_test_suite',
+        install_requires=[
+            'protobuf',
+        ],
+        classifiers=[
+            "Programming Language :: Python :: 3"
+        ],
+    )
