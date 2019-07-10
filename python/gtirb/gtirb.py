9cf1656d8c726cd707bfa75f415af809005222c3
Jeremy Lacomis <jlacomis@grammatech.com>
2019-07-10 14:51:12 -0400

    Move `AuxData` to its own file

--- python/gtirb/gtirb.py
+++ python/gtirb/gtirb.py
@@ -20,12 +20,10 @@ TODOS:
 * Support creating default instances for all classes.
 
 """
-import io
 
 from uuid import UUID, uuid4
 from enum import Enum
 
-import AuxData_pb2
 import AuxDataContainer_pb2
 import Block_pb2
 import ByteMap_pb2
@@ -39,50 +37,7 @@ import Section_pb2
 import Symbol_pb2
 import SymbolicExpression_pb2
 
-from gtirb.serialization import Serialization
-
-# The global serializer instance. User can use this to register new
-# encoders/decoders.
-serializer = Serialization()
-
-
-class AuxData:
-    """Types and operations for auxiliary data.  AuxData objects can be
-    attached to the IR or individual Modules to store additional
-    client-specific data in a portable way.
-    """
-
-    def __init__(self, data, type_name=None):
-        """Constructor
-        :param data: aux data
-        :param type_name: `str`, optional. Type of the data.
-        """
-        self.data = data
-        self.type_name = type_name
-
-    def _to_protobuf(self):
-        """Returns protobuf representation of the object
-
-        :returns: protobuf representation of the object
-        :rtype: protobuf object
-
-        """
-        ret = AuxData_pb2.AuxData()
-        out_bytes_array = io.BytesIO()
-        check_type_name = serializer.encode(out_bytes_array, self.data,
-                                            type_name_hint=self.type_name)
-        ret.type_name = check_type_name
-        out_bytes_array.seek(0)
-        ret.data = out_bytes_array.read()
-        return ret
-
-    @classmethod
-    def _from_protobuf(cls, aux_data, uuid_cache=None):
-        """
-        Load pygtirb class from protobuf class
-        """
-        ret = serializer.decode(aux_data.type_name, io.BytesIO(aux_data.data))
-        return cls(data=ret, type_name=aux_data.type_name)
+from gtirb.auxdata import AuxData
 
 
 class AuxDataContainer:
