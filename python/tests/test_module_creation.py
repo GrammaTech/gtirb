43278239f1c55142696db48bbc662b3940ada4a0
Jeremy Lacomis <jlacomis@grammatech.com>
2019-07-10 13:01:54 -0400

    Fix PEP8 in `test_module_creation.py`

--- python/tests/test_module_creation.py
+++ python/tests/test_module_creation.py
@@ -1,10 +1,12 @@
 import unittest
 from gtirb import Module
 
+
 class MainTest(unittest.TestCase):
     def test_module_create(self):
         mod = Module()
         assert mod is not None
+
 
 if __name__ == '__main__':
     unittest.main()
