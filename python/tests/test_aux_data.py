f6d6cfde4516f6a4b476930cea855fee50610113
Jeremy Lacomis <jlacomis@grammatech.com>
2019-07-10 13:03:20 -0400

    Fix PEP8 compliance in `test_aux_data.py`

--- python/tests/test_aux_data.py
+++ python/tests/test_aux_data.py
@@ -2,6 +2,7 @@ import os
 import unittest
 from gtirb import IR
 
+
 class AuxDataTest(unittest.TestCase):
     def test_ir_open(self):
 
@@ -11,21 +12,22 @@ class AuxDataTest(unittest.TestCase):
         ad = ir.aux_data['test']
         self.assertTrue(ad is not None)
         self.assertTrue(ad.type_name == 'mapping<string,UUID>')
-        self.assertTrue(sorted(list(ad.data.keys())) == ['BAR','FOO'])
+        self.assertTrue(sorted(list(ad.data.keys())) == ['BAR', 'FOO'])
 
         ir = IR.load_protobuf(os.path.join(test_path, 'test2.gtir'))
         ad = ir.aux_data['test']
         ad1 = ir.aux_data['test1']
         self.assertTrue(ad.type_name == 'sequence<UUID>')
         self.assertTrue(ad1.type_name == 'sequence<string>')
-        self.assertTrue(sorted(ad1.data) == ['BAR','FOO'])
+        self.assertTrue(sorted(ad1.data) == ['BAR', 'FOO'])
 
         ir = IR.load_protobuf(os.path.join(test_path, 'test3.gtir'))
         ad = ir.aux_data['test']
         self.assertTrue(ad is not None)
         self.assertTrue(ad.type_name == 'mapping<UUID,sequence<string>>')
-        self.assertTrue(sorted(list(ad.data.values())) ==
-                        [['BAZ', 'QUX'], ['FOO', 'BAR']])
+        self.assertTrue(
+            sorted(list(ad.data.values())) == [['BAZ', 'QUX'], ['FOO', 'BAR']]
+        )
 
 
 if __name__ == '__main__':
