b30d05580375096980d5f4b8462d91847231d2f4
Jeremy Lacomis <jlacomis@grammatech.com>
2019-07-10 13:04:24 -0400

    Fix PEP8 compliance in `test_ir_open.py`

--- python/tests/test_ir_open.py
+++ python/tests/test_ir_open.py
@@ -2,11 +2,13 @@ import os
 import unittest
 from gtirb import IR
 
+
 class MainTest(unittest.TestCase):
     def test_ir_open(self):
         test_path = os.path.dirname(os.path.realpath(__file__))
         ir = IR.load_protobuf(os.path.join(test_path, 'test4.gtir'))
         self.assertTrue(ir is not None)
+
 
 if __name__ == '__main__':
     unittest.main()
