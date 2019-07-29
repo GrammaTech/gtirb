832bd209b714cb3327bebc2695ad3cd62a5f9456
Jeremy Lacomis <jlacomis@cmu.edu>
2019-07-29 09:55:28 -0400

    Check type of class when deserializing from protobuf

--- python/gtirb/node.py
+++ python/gtirb/node.py
@@ -2,6 +2,12 @@ from uuid import UUID, uuid4
 from weakref import WeakValueDictionary
 
 
+class DecodeError(Exception):
+    """Exception during decoding"""
+    def __init__(self, msg):
+        self.msg = msg
+
+
 class Node:
     """Base class for 'nodes', which can be referenced by their UUID"""
     _uuid_cache = WeakValueDictionary()
@@ -23,7 +29,13 @@ class Node:
         """
         uuid = UUID(bytes=proto_object.uuid)
         if uuid in Node._uuid_cache:
-            return Node._uuid_cache[uuid]
+            node = Node._uuid_cache[uuid]
+            if not isinstance(node, cls):
+                node_type = node.__class__.__name__
+                cls_type = cls.__class__.__name__
+                raise DecodeError("%s corresponds to node of type %s, not %s" %
+                                  (uuid, node_type, cls_type))
+            return node
         new_node = cls._decode_protobuf(proto_object, uuid)
         Node._uuid_cache[new_node.uuid] = new_node
         return new_node
