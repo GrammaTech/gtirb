ff34d1542ce5c05012a60e1eb79a423f6bcc6007
Jeremy Lacomis <jlacomis@cmu.edu>
2019-07-30 08:30:54 -0400

    Fix filename in `test_ir_open.py`

--- python/tests/test_ir_open.py
+++ python/tests/test_ir_open.py
@@ -6,7 +6,7 @@ from gtirb import IR
 class MainTest(unittest.TestCase):
     def test_ir_open(self):
         test_path = os.path.dirname(os.path.realpath(__file__))
-        ir = IR.load_protobuf(os.path.join(test_path, 'test4.gtir'))
+        ir = IR.load_protobuf(os.path.join(test_path, 'test1.ir'))
         self.assertTrue(ir is not None)
 
 
