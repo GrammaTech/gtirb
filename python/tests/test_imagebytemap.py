ec70c50d07bd7293f4b2c7ae84ae94c5b08bd407
Jeremy Lacomis <jlacomis@cmu.edu>
2019-07-31 13:38:37 -0400

    Add docstrings and remove duplicate code in `test_imagebytemap.py`

--- python/tests/test_imagebytemap.py
+++ python/tests/test_imagebytemap.py
@@ -7,37 +7,38 @@ def decode_byte(byte):
 
 
 class TestImageByteMap(unittest.TestCase):
-    def test_initialization_coalescing(self):
-        byte_map = {10: b'aaaaa', 15: b'bbbbbbb',
-                    110: b'cccccc', 310: b'ffffff'}
-        ibm = ImageByteMap(addr_min=10,
-                           addr_max=315,
-                           base_address=10,
-                           byte_map=byte_map,
-                           entry_point_address=10,
-                           uuid=None)
+    """Test ImageByteMap"""
+
+    @staticmethod
+    def _new_ibm(byte_map, *, addr_min=0, addr_max=315, base_address=10,
+                 entry_point_address=10, uuid=None):
+        """Creates a new ImageByteMap using default arguments"""
+        return ImageByteMap(addr_min=addr_min,
+                            addr_max=addr_max,
+                            base_address=base_address,
+                            byte_map=byte_map,
+                            entry_point_address=entry_point_address,
+                            uuid=uuid)
+
+    def test_coalescing(self):
+        """Test coalescing of adjacent bytearrays during initialization"""
+        byte_map = \
+            {10: b'aaaaa', 15: b'bbbbbbb', 110: b'cccccc', 310: b'ffffff'}
+        ibm = TestImageByteMap._new_ibm(byte_map)
         self.assertTrue(len(ibm._byte_map) == 3)
 
-    def test_initialization_overlapping_ranges(self):
+    def test_overlapping_ranges(self):
+        """Test detection of overlapping bytearrays during initialization"""
         bad_byte_map = \
             {10: b'aaaaa', 11: b'bbbbbbb', 110: b'bbbbbb', 310: b'ffffff'}
         with self.assertRaises(ValueError, msg="Overlapping ranges uncaught"):
-            ImageByteMap(addr_min=10,
-                         addr_max=315,
-                         base_address=10,
-                         byte_map=bad_byte_map,
-                         entry_point_address=10,
-                         uuid=None)
+            TestImageByteMap._new_ibm(bad_byte_map)
 
     def test_contains(self):
-        byte_map = {10: b'aaaaa', 15: b'bbbbbbb',
-                    110: b'cccccc', 310: b'ffffff'}
-        ibm = ImageByteMap(addr_min=10,
-                           addr_max=315,
-                           base_address=10,
-                           byte_map=byte_map,
-                           entry_point_address=10,
-                           uuid=None)
+        """Test ImageByteMap.__contains__"""
+        byte_map = \
+            {10: b'aaaaa', 15: b'bbbbbbb', 110: b'cccccc', 310: b'ffffff'}
+        ibm = TestImageByteMap._new_ibm(byte_map)
         self.assertTrue(10 in ibm)
         self.assertTrue(15 in ibm)
         self.assertTrue(21 in ibm)
@@ -46,13 +47,9 @@ class TestImageByteMap(unittest.TestCase):
         self.assertFalse("test" in ibm)
 
     def test_delitem_single(self):
+        """Test ImageByteMap.__delitem__ with int index"""
         byte_map = {5: [0] * 5}
-        ibm = ImageByteMap(addr_min=0,
-                           addr_max=20,
-                           base_address=10,
-                           byte_map=byte_map,
-                           entry_point_address=10,
-                           uuid=None)
+        ibm = TestImageByteMap._new_ibm(byte_map, addr_min=0, addr_max=20)
         self.assertEqual(list(ibm), [(5, 0), (6, 0), (7, 0), (8, 0), (9, 0)])
         self.assertEqual(ibm._start_addresses, [5])
         del ibm[5]
@@ -64,20 +61,14 @@ class TestImageByteMap(unittest.TestCase):
         del ibm[7]
         self.assertEqual(list(ibm), [(6, 0), (8, 0)])
         self.assertEqual(ibm._start_addresses, [6, 8])
-
         with self.assertRaises(IndexError, msg="nonexistent key delete"):
             del ibm[20]
 
     def test_delitem_slice(self):
-
+        """Test ImageByteMap.__delitem__ with slice index"""
         def test_slice(del_slice, expected_list, expected_starts, msg):
             byte_map = {5: [0] * 5}
-            ibm = ImageByteMap(addr_min=0,
-                               addr_max=20,
-                               base_address=10,
-                               byte_map=byte_map,
-                               entry_point_address=10,
-                               uuid=None)
+            ibm = TestImageByteMap._new_ibm(byte_map, addr_min=0, addr_max=20)
             del ibm[del_slice]
             self.assertEqual(list(ibm), expected_list, msg=msg)
             self.assertEqual(ibm._start_addresses, expected_starts)
@@ -97,39 +88,11 @@ class TestImageByteMap(unittest.TestCase):
             test_slice(del_slice, expected_list, expected_starts, msg)
             test_index += 1
 
-        byte_map = {10: b'aaaaa', 15: b'bbbbbbb',
-                    110: b'cccccc', 310: b'ffffff'}
-        ibm = ImageByteMap(addr_min=10,
-                           addr_max=315,
-                           base_address=10,
-                           byte_map=byte_map,
-                           entry_point_address=10,
-                           uuid=None)
-
-        def index_error(test_slice, msg):
-            with self.assertRaises(IndexError, msg=msg):
-                ibm[test_slice]
-
-        bad_slices = (
-            (slice(0, 15, None), "start not in map"),
-            (slice(10, 50, None), "stop not in map"),
-            (slice(15, 10, None), "reverse slicing"),
-            (slice(15, 310, None), "gap in bytes"),
-            (slice(15, 16, 2), "slicing unsupported"),
-        )
-        for (test_slice, msg) in bad_slices:
-            index_error(test_slice, msg)
-
     def test_getitem(self):
-        byte_map = {10: b'aaaaa', 15: b'bbbbbbb',
-                    110: b'cccccc', 310: b'ffffff'}
-        ibm = ImageByteMap(addr_min=10,
-                           addr_max=315,
-                           base_address=10,
-                           byte_map=byte_map,
-                           entry_point_address=10,
-                           uuid=None)
-
+        """Test ImageByteMap.__getitem__"""
+        byte_map = \
+            {10: b'aaaaa', 15: b'bbbbbbb', 110: b'cccccc', 310: b'ffffff'}
+        ibm = TestImageByteMap._new_ibm(byte_map)
         self.assertEqual(ibm[10], decode_byte(b'a'))
         self.assertEqual(ibm[14], decode_byte(b'a'))
         self.assertEqual(ibm[15], decode_byte(b'b'))
@@ -158,15 +121,10 @@ class TestImageByteMap(unittest.TestCase):
             ibm["test"]
 
     def test_iter(self):
-        simple_byte_map = \
-            {0: b'aa', 10: b'bb', 12: b'cc'}
-        simple_ibm = ImageByteMap(addr_min=0,
-                                  addr_max=15,
-                                  base_address=0,
-                                  byte_map=simple_byte_map,
-                                  entry_point_address=10,
-                                  uuid=None)
-        self.assertEqual(list(simple_ibm),
+        """Test ImageByteMap.__iter__"""
+        byte_map = {0: b'aa', 10: b'bb', 12: b'cc'}
+        ibm = TestImageByteMap._new_ibm(byte_map, addr_min=0, addr_max=15)
+        self.assertEqual(list(ibm),
                          [(0, decode_byte(b'a')),
                           (1, decode_byte(b'a')),
                           (10, decode_byte(b'b')),
@@ -175,23 +133,15 @@ class TestImageByteMap(unittest.TestCase):
                           (13, decode_byte(b'c'))])
 
     def test_len(self):
-        byte_map = {10: b'aaaaa', 15: b'bbbbbbb',
-                    110: b'cccccc', 310: b'ffffff'}
-        ibm = ImageByteMap(addr_min=10,
-                           addr_max=315,
-                           base_address=10,
-                           byte_map=byte_map,
-                           entry_point_address=10,
-                           uuid=None)
+        """Test ImageByteMap.__len__"""
+        byte_map = \
+            {10: b'aaaaa', 15: b'bbbbbbb', 110: b'cccccc', 310: b'ffffff'}
+        ibm = TestImageByteMap._new_ibm(byte_map)
         self.assertEqual(len(ibm), 24)
 
     def test_setitem_single(self):
-        ibm = ImageByteMap(addr_min=5,
-                           addr_max=15,
-                           base_address=0,
-                           byte_map={},
-                           entry_point_address=10,
-                           uuid=None)
+        """Test ImageByteMap.__setitem__ with int index"""
+        ibm = TestImageByteMap._new_ibm(dict(), addr_min=5, addr_max=15)
         self.assertEqual(len(ibm), 0)
         ibm[10] = 0
         self.assertEqual(ibm[10], 0)
@@ -221,12 +171,8 @@ class TestImageByteMap(unittest.TestCase):
             ibm[20] = 0
 
     def test_setitem_slice(self):
-        ibm = ImageByteMap(addr_min=5,
-                           addr_max=15,
-                           base_address=0,
-                           byte_map={},
-                           entry_point_address=10,
-                           uuid=None)
+        """Test ImageByteMap.__setitem__ with slice index"""
+        ibm = TestImageByteMap._new_ibm(dict(), addr_min=5, addr_max=15)
         ibm[5:] = [1, 2, 3, 4, 5]
         self.assertEqual(ibm[5], 1)
         self.assertEqual(ibm[9], 5)
