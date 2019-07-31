5889dc51241757d274fab8d662f77011000e96bf
Jeremy Lacomis <jlacomis@cmu.edu>
2019-07-31 07:46:44 -0400

    Clean up documentation in `auxdata.py`

--- python/gtirb/auxdata.py
+++ python/gtirb/auxdata.py
@@ -16,34 +16,37 @@ class AuxData:
         serializer: A Serialization object used for encoding/decoding aux data
         data: data associated with this aux data
         type_name: optional string describing this aux data
-    """
 
+    """
     serializer = Serialization()
 
     def __init__(self, data, type_name=None):
-        """Constructor
-        :param data: aux data
-        :param type_name: `str`, optional. Type of the data.
+        """Create a new AuxData object
+
+        Parameters:
+            data: Aux data
+            type_name: Optional string. Type of the data.
+
         """
         self.data = data
         self.type_name = type_name
 
     @classmethod
     def _from_protobuf(cls, aux_data):
-        """
-        Load pygtirb class from protobuf class
+        """Deserialize AuxData from Protobuf.
+
+        Parameters:
+            cls: a Python AuxData instance
+            aux_data: the Protobuf AuxData object
+
         """
         data = AuxData.serializer.decode(BytesIO(aux_data.data),
                                          aux_data.type_name)
         return cls(data=data, type_name=aux_data.type_name)
 
     def _to_protobuf(self):
-        """Returns protobuf representation of the object
+        """Get a Protobuf representation of the AuxData"""
 
-        :returns: protobuf representation of the object
-        :rtype: protobuf object
-
-        """
         proto_auxdata = AuxData_pb2.AuxData()
         out_bytes_array = BytesIO()
         proto_auxdata.type_name = \
@@ -55,40 +58,26 @@ class AuxData:
 
 
 class AuxDataContainer(Node):
-    """Holds AuxData tables, base class for IR and Module
-    """
+    """Holds AuxData tables, base class for IR and Module"""
 
     def __init__(self, aux_data=dict(), uuid=None):
-        """Constructor
-        :param aux_data: dict(str, AuxData), optional dict mapping
-            type names to AuxData objects
-        :returns: AuxDataContainer
-        :rtype: AuxDataContainer
+        """Create a new AuxDataContainer
+
+        Parameters:
+            aux_data: dict(str, AuxData), optional dict mapping
+                type names to AuxData objects
+
         """
         super().__init__(uuid)
         self.aux_data = dict(aux_data)
 
     @classmethod
     def _decode_protobuf(cls, proto_container, uuid):
-        """Load pygtirb object from protobuf object
-
-        :param cls: this class
-        :param aux_data_container: protobuf object
-        :returns: pygtirb object
-        :rtype: AuxDataContainer
-
-        """
         aux_data = ((key, AuxData.from_protobuf(val))
                     for key, val in proto_container.aux_data.items())
         return cls(aux_data, uuid)
 
     def _to_protobuf(self):
-        """Returns protobuf representation of the object
-
-        :returns: protobuf representation of the object
-        :rtype: protobuf object
-
-        """
         proto_auxdatacontainer = AuxDataContainer_pb2.AuxDataContainer()
         for k, v in self.aux_data.items():
             proto_auxdatacontainer.aux_data[k].CopyFrom(v._to_protobuf())
