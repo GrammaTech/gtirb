f330166038d69d8e1e92cf1c864189c1e9264057
Jeremy Lacomis <jlacomis@cmu.edu>
2019-07-31 08:18:24 -0400

    Cleanup and documentation of `ir.py`

--- python/gtirb/ir.py
+++ python/gtirb/ir.py
@@ -1,16 +1,13 @@
-"""IR Module
-Sample usage.
+"""GTIRB IR module
 
-Opening a GTIR file and loading it into an IR instance
+Sample usage:
+    Opening a GTIRB Protobuf file and loading it into an IR instance:
+        ir = IR.load_protobuf('filename.gtir')
 
-    ir = IR.load_protobuf('filename.gtir')
-
-Writing back the ir instance as a protobuf file
-
-    IR.save_protobuf('filename.gtir')
+    Writing the IR instance as a Protobuf file:
+        IR.save_protobuf('filename.gtir')
 
 """
-
 import IR_pb2
 
 from .auxdata import AuxData, AuxDataContainer
@@ -18,41 +15,30 @@ from .module import Module
 
 
 class IR(AuxDataContainer):
-    """
-    A complete internal representation consisting of multiple Modules.
-    """
+    """A complete internal representation consisting of multiple Modules.
 
+    Attributes:
+        aux_data: Auxilary data associated with this IR
+        modules: list of Modules contained in the IR
+        uuid: the UUID of this Node
+
+    """
     def __init__(self, modules=list(), aux_data=dict(), uuid=None):
-        """IR constructor. Can be used to construct an empty IR instance
-
-        :param uuid: UUID. Creates a new instance if None
-        :param modules: List of modules
-        :param aux_data: auxilary data hanging off the IR
-        :returns: IR
-        :rtype: IR
-        """
+        # Note: modules are decoded before the aux data, since the UUID
+        # decoder checks Node's cache.
         self.modules = list(modules)
         super().__init__(aux_data, uuid)
 
     @classmethod
     def _decode_protobuf(cls, proto_ir, uuid):
-        # Note: modules are decoded before the aux data, since the UUID
-        # decoder checks Node's cache.
-        modules = [Module._from_protobuf(m)
-                   for m in proto_ir.modules]
-        aux_data = {
-            key: AuxData._from_protobuf(val)
+        aux_data = (
+            (key, AuxData._from_protobuf(val))
             for key, val in proto_ir.aux_data_container.aux_data.items()
-        }
+        )
+        modules = (Module._from_protobuf(m) for m in proto_ir.modules)
         return cls(modules, aux_data, uuid)
 
     def _to_protobuf(self):
-        """Returns protobuf representation of the object
-
-        :returns: protobuf representation of the object
-        :rtype: protobuf object
-
-        """
         proto_ir = IR_pb2.IR()
         proto_ir.uuid = self.uuid.bytes
         proto_ir.modules.extend(m._to_protobuf() for m in self.modules)
@@ -61,11 +47,13 @@ class IR(AuxDataContainer):
 
     @staticmethod
     def load_protobuf_file(protobuf_file):
-        """Load IR from protobuf file object
+        """Load IR from Protobuf file object
 
-        :param protobuf_file: The protobuf file object
-        :returns: GTIR
-        :rtype: IR
+        Parameters:
+            protobuf_file: a file object containing the Protobuf
+
+        Returns:
+            Python GTIRB IR object
 
         """
         ir = IR_pb2.IR()
@@ -74,26 +62,33 @@ class IR(AuxDataContainer):
 
     @staticmethod
     def load_protobuf(file_name):
-        """Load IR from protobuf file at path.
+        """Load IR from Protobuf file at path
 
-        :param file_name: The given protobuf GTIR file path
-        :returns: GTIR
-        :rtype: IR
+        Parameters:
+            file_name: the path to the Protobuf file
+
+        Returns:
+            Python GTIRB IR object
+
         """
         with open(file_name, 'rb') as f:
             return IR.load_protobuf_file(f)
 
     def save_protobuf_file(self, protobuf_file):
-        """Save IR to protobuf file object
+        """Save IR to Protobuf file object
 
-        :param protobuf_file: The protobuf file object
+        Parameters:
+            protobuf_file: target file object
+
         """
         protobuf_file.write(self._to_protobuf().SerializeToString())
 
     def save_protobuf(self, file_name):
-        """Save IR to protobuf file at path.
+        """Save IR to Protobuf file at path
 
-        :param file_name: The given protobuf GTIR file path
+        Parameters:
+            file_name: the target path of the Protobuf file
+
         """
         with open(file_name, 'wb') as f:
             self.save_protobuf_file(f)
