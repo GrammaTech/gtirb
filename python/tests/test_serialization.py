7e8469e8e6723eb2acd5ee9010c8045198442199
Joshua Robbins <jrobbins@grammatech.com>
2019-09-25 12:03:16 -0400

    Added test for unknown codecs

--- python/tests/test_serialization.py
+++ python/tests/test_serialization.py
@@ -1,12 +1,15 @@
 import unittest
-from gtirb.serialization import Serialization, TypeNameError
+import gtirb.serialization
+import io
 
 
 class TestSerialization(unittest.TestCase):
     def test_parse_type(self):
         def test_positive(type_name, oracle):
-            self.assertEqual(Serialization._parse_type(type_name), oracle)
-
+            self.assertEqual(
+                gtirb.serialization.Serialization._parse_type(type_name),
+                oracle,
+            )
         positive_tests = [
             ("mapping", ("mapping", ())),
             ("mapping<FOO,BAR>", ("mapping", (("FOO", ()), ("BAR", ())))),
@@ -33,8 +36,11 @@ class TestSerialization(unittest.TestCase):
             test_positive(type_name, oracle)
 
         def test_negative(type_name):
-            with self.assertRaises(TypeNameError, msg=type_name):
-                Serialization._parse_type(type_name)
+            with self.assertRaises(
+                gtirb.serialization.TypeNameError,
+                msg=type_name,
+            ):
+                gtirb.serialization.Serialization._parse_type(type_name)
 
         negative_tests = [
             "mapping<<>",
@@ -48,6 +54,36 @@ class TestSerialization(unittest.TestCase):
         ]
         for type_name in negative_tests:
             test_negative(type_name)
+
+    def test_unknown_codec(self):
+        serializer = gtirb.serialization.Serialization()
+        blob = serializer.decode(b"abcd", "foobar")
+        self.assertIsInstance(blob, gtirb.serialization.UnknownData)
+        self.assertEqual(blob, b"abcd")
+        ostream = io.BytesIO()
+        serializer.encode(ostream, blob, "foobar")
+        self.assertEqual(ostream.getvalue(), b"abcd")
+
+    def test_nested_unknown_codec(self):
+        serializer = gtirb.serialization.Serialization()
+        ostream = io.BytesIO()
+        serializer.encode(ostream, {
+            "a": ["b", "c"],
+            "d": ["e"],
+        }, "mapping<string,sequence<string>>")
+        raw_bytes = ostream.getvalue()
+
+        blob = serializer.decode(raw_bytes, "mapping<string,sequence<foobar>>")
+        self.assertIsInstance(blob, gtirb.serialization.UnknownData)
+        ostream = io.BytesIO()
+        serializer.encode(ostream, blob, "mapping<string,sequence<foobar>>")
+        self.assertEqual(ostream.getvalue(), raw_bytes)
+
+        blob = serializer.decode(raw_bytes, "mapping<foobar,sequence<string>>")
+        self.assertIsInstance(blob, gtirb.serialization.UnknownData)
+        ostream = io.BytesIO()
+        serializer.encode(ostream, blob, "mapping<foobar,sequence<string>>")
+        self.assertEqual(ostream.getvalue(), raw_bytes)
 
 
 if __name__ == "__main__":
