c922919a736c2eeb05c9c1b782531dd354010b69
Eric Schulte <eschulte@grammatech.com>
2019-10-22 16:15:42 -0400

    Initial tests CL reading/writing of GTIRB protobuf

--- cl/gtirb.lisp
+++ cl/gtirb.lisp
@@ -1,10 +1,10 @@
 (defpackage :gtirb/gtirb
   (:nicknames :gtirb)
   (:use :common-lisp)
-  (:export :read-gtirb))
+  (:export :read-gtirb-proto :write-gtirb-proto))
 (in-package :gtirb/gtirb)
 
-(defun read-gtirb (path)
+(defun read-gtirb-proto (path)
   "Read GTIRB from PATH."
   (assert (probe-file path) (path)
           "Can't read GTIRB from ~s, because the file doesn't exist."
@@ -17,3 +17,14 @@
         (read-sequence buffer input)
         (pb:merge-from-array gtirb buffer 0 size)))
     gtirb))
+
+(defun write-gtirb-proto (gtirb path)
+  "Write GTIRB to PATH."
+  (let* ((size (pb:octet-size gtirb))
+         (buffer (make-array size :element-type '(unsigned-byte 8))))
+    (pb:serialize gtirb buffer 0 size)
+    (with-open-file (output path
+                            :direction :output :if-exists :supersede
+                            :element-type 'unsigned-byte)
+      (write-sequence buffer output)))
+  (values))
--- cl/test.lisp
+++ cl/test.lisp
@@ -1,9 +1,46 @@
 (defpackage :gtirb/test
   (:use :common-lisp :gtirb :stefil)
+  (:import-from :md5 :md5sum-file)
+  (:import-from :uiop :run-program :with-temporary-file)
   (:export :gtirb))
 (in-package :gtirb/test)
 
+(defvar *proto-path* nil "Path to protobuf.")
+
+
+;;;; Fixtures.
+(defixture hello
+  (:setup (progn
+            (let ((gtirb-path (with-temporary-file (:pathname p :keep t) p)))
+              (with-temporary-file (:pathname bin-path)
+                (run-program (format nil "echo 'main(){puts(\"hello world\");}'~
+                                           |gcc -x c - -o ~a"
+                                     bin-path) :force-shell t)
+                (run-program (format nil "ddisasm --ir ~a ~a" gtirb-path bin-path))
+                (delete-file bin-path))
+              (setf *proto-path* gtirb-path))))
+  (:teardown (progn
+               (delete-file *proto-path*)
+               (setf *proto-path* nil))))
+
+
+;;;; Main test suite.
 (defsuite gtirb)
 
 (deftest simple-read ()
-  (is t))
+  (with-fixture hello
+    (is (eql 'proto:ir
+             (class-name (class-of (read-gtirb-proto *proto-path*)))))))
+
+(deftest simple-write ()
+  (with-fixture hello
+    (with-temporary-file (:pathname path)
+      (write-gtirb-proto (read-gtirb-proto *proto-path*) path)
+      (is (probe-file path)))))
+
+(deftest idempotent-read-write ()
+  (with-fixture hello
+    (with-temporary-file (:pathname path)
+      (write-gtirb-proto (read-gtirb-proto *proto-path*) path)
+      (is (equalp (md5sum-file *proto-path*)
+                  (md5sum-file path))))))
