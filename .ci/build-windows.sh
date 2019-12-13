19764b5ce742ab96f3a840ae0b244f0804385ecc
Brian Fairservice <bfairservice@grammatech.com>
2019-12-13 09:23:15 -0500

    Move windows ctest into gitlab-ci.yml
    ip:debloat

--- .ci/build-windows.sh
+++ .ci/build-windows.sh
@@ -5,4 +5,3 @@ mkdir build
 cd build
 cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && C:\\PROGRA~1\\CMake\\bin\\cmake.exe -G "Ninja" -DBOOST_ROOT=\"C:\\Boost\" -DCMAKE_PREFIX_PATH=\"C:\\Program Files (x86)\\protobuf\" -DCMAKE_BUILD_TYPE=${BUILD_TYPE} .."
 cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && ninja"
-cmd.exe /C "C:\\PROGRA~1\\CMake\\bin\\ctest.exe"
