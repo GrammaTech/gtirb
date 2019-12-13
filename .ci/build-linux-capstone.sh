bd6db98c06cdfff01ece0859faab842ba7886ba3
Brian Fairservice <bfairservice@grammatech.com>
2019-12-13 09:14:16 -0500

    Move linux capstone installation to build script
    ip:debloat

--- .ci/build-linux-capstone.sh
+++ .ci/build-linux-capstone.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Install capstone
+cd /usr/local/src
+wget https://github.com/aquynh/capstone/archive/4.0.1.tar.gz
+tar xf 4.0.1.tar.gz
+cd capstone-4.0.1
+CAPSTONE_ARCHS=x86 ./make.sh
+CAPSTONE_ARCHS=x86 ./make.sh install
+
+# Build GTIRB
 cd /gt/gtirb/build
 cmake ../ -DCMAKE_CXX_COMPILER=${CXX_COMPILER}
 make -j
