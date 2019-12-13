7adce53b49d085be4a5ddfb384e96c7122a0c4ff
Brian Fairservice <bfairservice@grammatech.com>
2019-12-13 10:07:07 -0500

    Fix discrepancies w/ windows capstone build script
    ip:debloat

--- .ci/build-windows-capstone.sh
+++ .ci/build-windows-capstone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BUILD_TYPE=$1
 
-# Build and install capstone
+# Install capstone
 pushd $HOME
 mkdir /cygdrive/C/capstone-$BUILD_TYPE
 wget https://github.com/aquynh/capstone/archive/4.0.1.tar.gz
@@ -9,9 +9,14 @@ tar xf 4.0.1.tar.gz
 cd capstone-4.0.1
 mkdir build
 cd build
-cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && ""C:\\Program Files\\CMake\\bin\\cmake.exe"" -G ""Ninja"" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=C:\\capstone-release .."
+cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && ""C:\\Program Files\\CMake\\bin\\cmake.exe"" -G ""Ninja"" -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=C:\\capstone-${BUILD_TYPE} .."
 cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && ninja"
 cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && ninja install"
 popd
 
-./.ci/build-windows.sh $BUILD_TYPE
+# Build GTIRB
+cd build
+cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && C:\\PROGRA~1\\CMake\\bin\\cmake.exe -G "Ninja" -DBOOST_ROOT=\"C:\\Boost\" -DCMAKE_PREFIX_PATH=\"C:\\Program Files (x86)\\protobuf\" -DCMAKE_BUILD_TYPE=${BUILD_TYPE} .."
+cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && ninja"
+
+./bin/TestGTIRB.exe
