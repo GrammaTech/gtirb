bfa3c70470bb1ef93f942ada680e555d3b99d8d4
Brian Fairservice <bfairservice@grammatech.com>
2019-12-17 13:57:12 -0500

    Remove cmake files as in linux build
    ip:debloat

--- .ci/build-windows.sh
+++ .ci/build-windows.sh
@@ -4,6 +4,7 @@
 
 BUILD_TYPE=$1
 
+rm -rf build CMakeCache.txt CMakeFiles CMakeScripts
 mkdir build
 cd build
 cmd.exe /C "C:\\VS\\VC\\Auxiliary\\Build\\vcvars64.bat && C:\\PROGRA~1\\CMake\\bin\\cmake.exe -G "Ninja" -DBOOST_ROOT=\"C:\\Boost\" -DCMAKE_PREFIX_PATH=\"C:\\Program Files (x86)\\protobuf\" -DCMAKE_BUILD_TYPE=${BUILD_TYPE} .."
