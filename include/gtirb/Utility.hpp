1e283ff7a31c4adc9ca894ef140092194e0ab064
Joshua Robbins <jrobbins@grammatech.com>
2020-01-16 15:58:01 -0500

    Documented Utility.hpp

--- include/gtirb/Utility.hpp
+++ include/gtirb/Utility.hpp
@@ -25,6 +25,14 @@
 
 namespace gtirb {
 
+/// @cond INTERNAL
+
+/// \class AddressOrder
+///
+/// \brief A comparison function object for comparing nodes in address order.
+///
+/// \tparam T The type to compare addresses with. Must have a method with the
+///           signature "std::optional<Addr> getAddress() const".
 template <typename T> struct AddressOrder {
   using key_type = std::optional<Addr>;
   static key_type key(const T& N) { return N.getAddress(); }
@@ -33,6 +41,11 @@ template <typename T> struct AddressOrder {
   }
 };
 
+/// \class BlockAddressOrder
+///
+/// \brief A comparison function object for comparing blocks (that is, \ref Node
+/// objects that are either \ref CodeBlock or \ref DastaBlock objects) in
+/// address order.
 struct GTIRB_EXPORT_API BlockAddressOrder {
   using key_type = std::optional<Addr>;
   static key_type getAddress(const Node* N);
@@ -41,10 +54,26 @@ struct GTIRB_EXPORT_API BlockAddressOrder {
   }
 };
 
+/// \class ArbitraryOrder
+///
+/// \brief A comparison function object for comparing types in an arbitrary yet
+/// stable manner.
+///
+/// \tparam T Any type.
 template <typename T> struct ArbitraryOrder {
   bool operator()(const T& N1, const T& N2) const { return &N1 < &N2; }
 };
 
+/// \class NodeToChildRange
+///
+/// \brief A function object for constructing \ref MergeSortedIterator objects
+/// via Boost transform iterators.
+///
+/// \tparam T The type to retrieve a range of children nodes from.
+/// \tparam Method A pointer-to-method type, taking no arguments and retuning
+///                a child node iterator.
+/// \tparam Begin  A pointer-to-method of the beginning of the child node range.
+/// \tparam End    A pointer-to-method of the end of the child node range.
 template <typename T, typename Method, Method Begin, Method End>
 struct NodeToChildRange {
   boost::iterator_range<decltype((std::declval<T>().*Begin)())>
@@ -53,6 +82,15 @@ struct NodeToChildRange {
   }
 };
 
+/// \class NodeToBlockRange
+///
+/// \brief A function object for constructing \ref MergeSortedIterator objects
+/// via Boost transform iterators. Returns ranges of \ref Node objects, which
+/// are either \ref CodeBlock objects or \ref DataBlock objects.
+///
+/// \tparam T The type to retrieve ranges of blocks from. If const-qualified,
+///           the const iterators on this type are used; else the non-const
+///           iterators are used.
 template <typename T>
 using NodeToBlockRange = NodeToChildRange<
     T,
@@ -61,6 +99,14 @@ using NodeToBlockRange = NodeToChildRange<
                        typename T::block_iterator (T::*)()>,
     &T::blocks_begin, &T::blocks_end>;
 
+/// \class NodeToCodeBlockRange
+///
+/// \brief A function object for constructing \ref MergeSortedIterator objects
+/// via Boost transform iterators. Returns ranges of \ref CodeBlock objects.
+///
+/// \tparam T The type to retrieve ranges of blocks from. If const-qualified,
+///           the const iterators on this type are used; else the non-const
+///           iterators are used.
 template <typename T>
 using NodeToCodeBlockRange = NodeToChildRange<
     T,
@@ -69,6 +115,14 @@ using NodeToCodeBlockRange = NodeToChildRange<
                        typename T::code_block_iterator (T::*)()>,
     &T::code_blocks_begin, &T::code_blocks_end>;
 
+/// \class NodeToDataBlockRange
+///
+/// \brief A function object for constructing \ref MergeSortedIterator objects
+/// via Boost transform iterators. Returns ranges of \ref DataBlock objects.
+///
+/// \tparam T The type to retrieve ranges of blocks from. If const-qualified,
+///           the const iterators on this type are used; else the non-const
+///           iterators are used.
 template <typename T>
 using NodeToDataBlockRange = NodeToChildRange<
     T,
@@ -77,6 +131,16 @@ using NodeToDataBlockRange = NodeToChildRange<
                        typename T::data_block_iterator (T::*)()>,
     &T::data_blocks_begin, &T::data_blocks_end>;
 
+/// \class NodeToSymbolicExpressionRange
+///
+/// \brief A function object for constructing \ref MergeSortedIterator objects
+/// via Boost transform iterators. Returns ranges of \ref
+/// ByteInterval::SymbolicExpressionElement or \ref
+/// ByteInterval::ConstSymbolicExpressionElement objects.
+///
+/// \tparam T The type to retrieve ranges of symbolic expressions from. If
+///           const-qualified, the const iterators on this type are used; else
+///           the non-const iterators are used.
 template <typename T>
 using NodeToSymbolicExpressionRange = NodeToChildRange<
     T,
@@ -86,6 +150,14 @@ using NodeToSymbolicExpressionRange = NodeToChildRange<
                        typename T::symbolic_expression_iterator (T::*)()>,
     &T::symbolic_expressions_begin, &T::symbolic_expressions_end>;
 
+/// \class NodeToByteIntervalRange
+///
+/// \brief A function object for constructing \ref MergeSortedIterator objects
+/// via Boost transform iterators. Returns ranges of \ref ByteInterval objects.
+///
+/// \tparam T The type to retrieve ranges of intervals from. If const-qualified,
+///           the const iterators on this type are used; else the non-const
+///           iterators are used.
 template <typename T>
 using NodeToByteIntervalRange = NodeToChildRange<
     T,
@@ -94,6 +166,14 @@ using NodeToByteIntervalRange = NodeToChildRange<
                        typename T::byte_interval_iterator (T::*)()>,
     &T::byte_intervals_begin, &T::byte_intervals_end>;
 
+/// \class NodeToSymbolRange
+///
+/// \brief A function object for constructing \ref MergeSortedIterator objects
+/// via Boost transform iterators. Returns ranges of \ref Symbol objects.
+///
+/// \tparam T The type to retrieve ranges of symbols from. If const-qualified,
+///           the const iterators on this type are used; else the non-const
+///           iterators are used.
 template <typename T>
 using NodeToSymbolRange = NodeToChildRange<
     T,
@@ -102,6 +182,14 @@ using NodeToSymbolRange = NodeToChildRange<
                        typename T::symbol_iterator (T::*)()>,
     &T::symbols_begin, &T::symbols_end>;
 
+/// \class NodeToSectionRange
+///
+/// \brief A function object for constructing \ref MergeSortedIterator objects
+/// via Boost transform iterators. Returns ranges of \ref Section objects.
+///
+/// \tparam T The type to retrieve ranges of sections from. If const-qualified,
+///           the const iterators on this type are used; else the non-const
+///           iterators are used.
 template <typename T>
 using NodeToSectionRange = NodeToChildRange<
     T,
@@ -110,6 +198,14 @@ using NodeToSectionRange = NodeToChildRange<
                        typename T::section_iterator (T::*)()>,
     &T::sections_begin, &T::sections_end>;
 
+/// \class NodeToProxyBlockRange
+///
+/// \brief A function object for constructing \ref MergeSortedIterator objects
+/// via Boost transform iterators. Returns ranges of \ref ProxyBlock objects.
+///
+/// \tparam T The type to retrieve ranges of blocks from. If const-qualified,
+///           the const iterators on this type are used; else the non-const
+///           iterators are used.
 template <typename T>
 using NodeToProxyBlockRange = NodeToChildRange<
     T,
@@ -117,6 +213,7 @@ using NodeToProxyBlockRange = NodeToChildRange<
                        typename T::const_proxy_block_iterator (T::*)() const,
                        typename T::proxy_block_iterator (T::*)()>,
     &T::proxy_blocks_begin, &T::proxy_blocks_end>;
+/// @endcond
 
 } // namespace gtirb
 
