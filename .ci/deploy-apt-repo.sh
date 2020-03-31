42cb27d165a201edfb6e6f897e86d327a78dc661
Brian Fairservice <bfairservice@grammatech.com>
2020-03-31 15:49:27 -0400

    Create deploy-apt-repo.sh
    This script creates two apt repositories under the given directory,
    which contain our GTIRB packages.

---
+++ .ci/deploy-apt-repo.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# deploy-apt-repo.sh
+#
+# Usage:
+#   ./deploy_directory.sh deploy_directory
+#
+# Summary:
+#   Creates two apt repositories underneath deploy_directory (one for xenial,
+#   one for bionic).  The created directory structure is:
+#     - deploy_directory
+#       - xenial
+#         - [ubuntu16 packages]
+#         - Packages.gz
+#       - bionic
+#         - [ubuntu18 packages]
+#         - Packages.gz
+#
+#   deploy_directory gets completely whiped out and recreated by this script,
+#   so be careful.  For the moment, the only package we include is gtirb.  We
+#   pull the package from the master branch's job artifacts.
+#
+#   In order to install packages from the created repositories, one would add
+#   one of the above directories to their /etc/apt/sources.list file (see the
+#   'Build' section of README.md).
+
+set -o xtrace
+set -o nounset
+set -o errexit
+set -o pipefail
+
+DEPLOY_DIR=$1
+
+rm -rf $DEPLOY_DIR
+mkdir -p $DEPLOY_DIR
+
+function setup_repo {
+  local OS_NUM=$1
+  local CODENAME=$2
+  mkdir $DEPLOY_DIR/$CODENAME
+  pushd $DEPLOY_DIR/$CODENAME
+  curl -L https://git.grammatech.com/rewriting/gtirb/-/jobs/artifacts/master/download?job=debian-installer-ubuntu$OS_NUM --output "gtirb-artifacts.zip"
+  unzip gtirb-artifacts.zip
+  rm *.zip
+  dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
+  popd  
+};
+  
+setup_repo 16 xenial
+setup_repo 18 bionic
