372f41f2db701b03d9907bdff0bc72769493db6f
Eric Schulte <eschulte@grammatech.com>
2020-07-09 10:54:20 -0400

    Add Ubuntu 20 support to CI

--- .ci/deploy-repos.sh
+++ .ci/deploy-repos.sh
@@ -5,9 +5,8 @@
 # Usage:
 #   ./deploy_repos.sh deploy_directory/
 #
-# Summary:
-#   Creates two apt repositories underneath deploy_directory (one for xenial,
-#   one for bionic).  The created directory structure is:
+# Summary: Creates three apt repositories underneath deploy_directory
+#   (for xenial, bionic, and focal).  The created directory structure is:
 #     - deploy_directory
 #       - xenial
 #         - [ubuntu16 packages]
@@ -42,6 +41,7 @@ function setup_repo {
   case $OS in
     ubuntu16) JOB_NAME='debian-installer-ubuntu16' ;;
     ubuntu18) JOB_NAME='debian-installer-ubuntu18' ;;
+    ubuntu20) JOB_NAME='debian-installer-ubuntu20' ;;
     arch) JOB_NAME='package-arch' ;;
     windows-debug) JOB_NAME='build-windows-msvc-debug';;
     windows-release) JOB_NAME='build-windows-msvc-relwithdebinfo';;
@@ -50,7 +50,7 @@ function setup_repo {
     curl -L https://git.grammatech.com/rewriting/${PROJECT}/-/jobs/artifacts/master/download?job=${JOB_NAME} \
          --output "${PROJECT}-artifacts.zip"
     case $OS in
-      arch|ubuntu16|ubuntu18)
+      arch|ubuntu16|ubuntu18|ubuntu20)
         unzip ${PROJECT}-artifacts.zip
         rm *.zip
         ;;
@@ -62,7 +62,7 @@ function setup_repo {
     esac
   done
   case $OS in
-    ubuntu16|ubuntu18)
+    ubuntu16|ubuntu18|ubuntu20)
       curl http://otsego.grammatech.com/u4/TARBALLS/debloat/pkgs/libcapstone-dev_4.0.1-gt1_amd64.deb \
            --output libcapstone-dev_4.0.1-gt1_amd64.deb
       dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
@@ -75,6 +75,7 @@ rm -rf /tmp/pdb/
 
 setup_repo ubuntu16 xenial
 setup_repo ubuntu18 bionic
+setup_repo ubuntu20 focal
 setup_repo arch arch
 setup_repo windows-debug windows-debug
 setup_repo windows-release windows-release
