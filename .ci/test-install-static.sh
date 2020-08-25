dd9207ec371d306c03a16a64ce2b895890f16ba1
Joshua Robbins <jrobbins@grammatech.com>
2020-08-25 09:51:27 -0400

    Add more test and checks for static GTIRB

---
+++ .ci/test-install-static.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+set -o xtrace
+set -o nounset
+set -o errexit
+
+builddir=$(pwd)
+workdir=`mktemp -d`
+trap 'cd / ; rm -rf $workdir' EXIT
+cd $workdir
+
+# Compile and run a C++ file that links to libgtirb statically
+cp $builddir/.ci/test-install.cpp ./
+make 'CXXFLAGS=-std=c++17' 'LDLIBS=-lgtirb -lgtirb_proto -lprotobuf' test-install
+./test-install
