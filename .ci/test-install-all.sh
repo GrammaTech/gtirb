b5bbf2790bdf98ac82820664a1872a258995dd16
Jonathan Dorn <jdorn@grammatech.com>
2021-12-20 15:05:48 -0500

    Remove stale references to install-python target

--- .ci/test-install-all.sh
+++ .ci/test-install-all.sh
@@ -13,9 +13,3 @@ cd $workdir
 cp $builddir/.ci/test-install.cpp ./
 make CXXFLAGS=-std=c++17 LDLIBS=-lgtirb test-install
 ./test-install
-
-# Test that the Python package was installed
-ls -l -R $workdir
-python3 -m unittest discover -s $builddir/python/tests
-cp $builddir/.ci/test-install.py ./
-python3 test-install.py
