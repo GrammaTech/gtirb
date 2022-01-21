188186e8a22901b3f3156df6a67475acf470a27f
Ariel Davis <adavis@grammatech.com>
2022-01-21 14:12:32 -0600

    removed AuxData interop test from ctest

--- .ci/test-interop.sh
+++ .ci/test-interop.sh
@@ -1,7 +1,7 @@
 set -e
-@test_floats@ -w floats_cpp.gtirb
+bin/test_floats -w floats_cpp.gtirb
 python3 test_floats.py -w floats_py.gtirb
 
-@test_floats@ -r floats_py.gtirb
+bin/test_floats -r floats_py.gtirb
 python3 test_floats.py -r floats_cpp.gtirb
 rm floats_*.gtirb
