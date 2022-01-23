944668f9ae3004d29b255a8e84e47cf3cad5e1f2
Ariel Davis <adavis@grammatech.com>
2022-01-23 14:36:18 -0600

    fixing typoes

--- .ci/test-interop.sh
+++ .ci/test-interop.sh
@@ -1,7 +1,10 @@
 set -e
+cd build
 bin/test_floats -w floats_cpp.gtirb
-python3 test_floats.py -w floats_py.gtirb
+python3 src/test/testInterop/test_floats.py -w floats_py.gtirb
 
-bin/test_floats -r floats_py.gtirb
-python3 test_floats.py -r floats_cpp.gtirb
+bin/test_floats -r floats_py.gtirb && \
+    echo "python float AuxData work in c++"
+python3 src/test/testInterop/test_floats.py -r floats_cpp.gtirb && \
+    echo "c++ float AuxData work in python"
 rm floats_*.gtirb
