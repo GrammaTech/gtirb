5c5809f1d63c9ef60cbb875b7186e67f806f0ed2
Ariel Davis <adavis@grammatech.com>
2022-05-11 15:36:03 -0500

    added interop test for variants

--- .ci/test-interop.sh
+++ .ci/test-interop.sh
@@ -15,7 +15,7 @@ rm floats_*.gtirb
 # variant compatibility test
 
 bin/test_variants -w variants_cpp.gtirb
-python3 python3 src/test/testInterop/test_variants.py -w variants_py.gtirb
+python3 src/test/testInterop/test_variants.py -w variants_py.gtirb
 python3 src/test/testInterop/test_variants.py -r variants_cpp.gtirb && \
     echo "c++ variant AuxData work in python"
 bin/test_variants -r variants_py.gtirb && \
