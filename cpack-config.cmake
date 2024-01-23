ecb058de442b3aa381b9c708cbe9647595255833
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2024-01-23 13:48:54 -0500

    include version in .deb file name

--- cpack-config.cmake
+++ cpack-config.cmake
@@ -11,7 +11,7 @@ set(DEV_COMPONENTS headers proto_library cmake_config cmake_target)
 # Debian packages
 if("${CPACK_GTIRB_PACKAGE}" STREQUAL "debian-lib")
   set(CPACK_DEBIAN_PACKAGE_NAME "libgtirb")
-  set(CPACK_PACKAGE_FILE_NAME "libgtirb")
+  set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
   set(CPACK_COMPONENTS_ALL ${LIB_COMPONENTS})
   if("${CPACK_DEBIAN_PACKAGE_RELEASE}" STREQUAL "focal")
     set(CPACK_DEBIAN_PACKAGE_DEPENDS
@@ -24,14 +24,14 @@ if("${CPACK_GTIRB_PACKAGE}" STREQUAL "debian-lib")
   endif()
 elseif("${CPACK_GTIRB_PACKAGE}" STREQUAL "debian-dev")
   set(CPACK_DEBIAN_PACKAGE_NAME "libgtirb-dev")
-  set(CPACK_PACKAGE_FILE_NAME "libgtirb-dev")
+  set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
   set(CPACK_COMPONENTS_ALL ${DEV_COMPONENTS})
   set(CPACK_DEBIAN_PACKAGE_DEPENDS
       "libstdc++6, libc6, libgcc1, libgtirb (=${CPACK_GTIRB_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}), libboost-dev (>=1.67) | libboost1.67-dev, libprotobuf-dev (>=${CPACK_PROTOBUF_VERSION_LOWER_BOUND}~), libprotobuf-dev (<<${CPACK_PROTOBUF_VERSION_UPPER_BOUND})"
   )
 elseif("${CPACK_GTIRB_PACKAGE}" STREQUAL "debian-debug")
   set(CPACK_DEBIAN_PACKAGE_NAME "libgtirb-dbg")
-  set(CPACK_PACKAGE_FILE_NAME "libgtirb-dbg")
+  set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
   set(CPACK_COMPONENTS_ALL debug-file)
   set(CPACK_DEBIAN_PACKAGE_DEPENDS
       "libgtirb (=${CPACK_GTIRB_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE})"
