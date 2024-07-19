3322cd9efd973c299eb2c91219d37a134f226186
Jonathan Dorn <jdorn@grammatech.com>
2024-07-19 17:20:50 -0400

    Try using relative_files in python report

--- python/setup.py
+++ python/setup.py
@@ -1,36 +1,11 @@
-import glob
 import imp
+
 import setuptools
-import sys
-import unittest
-import os
-import shutil
-
-
-class UseUnitTest(unittest.TestCase):
-    def runTest(self):
-        self.fail(
-            "Running tests with setup.py is not supported because it installs "
-            "dependencies incorrectly. Please use unittest directly instead."
-        )
-
-
-def gtirb_test_suite():
-    test_suite = unittest.TestSuite()
-    test_suite.addTest(UseUnitTest())
-    return test_suite
-
 
 # copy over files needed for sdist: README and LICENCE
-root_dir = "@CMAKE_SOURCE_DIR@"
-this_dir = os.path.dirname(__file__)
-shutil.copyfile(os.path.join(root_dir, "README.md"), "README")
-shutil.copyfile(os.path.join(root_dir, "LICENSE.txt"), "LICENSE")
 
 # Set the version
-version = imp.load_source(
-    "pkginfo.version", "gtirb/version.py"
-).API_VERSION
+version = imp.load_source("pkginfo.version", "gtirb/version.py").API_VERSION
 
 # run setuptools
 if __name__ == "__main__":
@@ -49,7 +24,6 @@ if __name__ == "__main__":
         description="GrammaTech Intermediate Representation for Binaries",
         packages=setuptools.find_packages(),
         package_data={"gtirb": ["py.typed"]},
-        test_suite="setup.gtirb_test_suite",
         install_requires=[
             "networkx",
             "protobuf<=3.20.1",
@@ -59,7 +33,11 @@ if __name__ == "__main__":
         ],
         extras_require={
             "doc": ["sphinx", "sphinx-autodoc-typehints"],
-            "dev": ["mypy==0.961", "mypy-protobuf==3.3.0", "types-protobuf==3.20.4"],
+            "dev": [
+                "mypy==0.961",
+                "mypy-protobuf==3.3.0",
+                "types-protobuf==3.20.4",
+            ],
         },
         classifiers=[
             "Programming Language :: Python :: 3",
